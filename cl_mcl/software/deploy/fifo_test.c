d394ac4c6a1b085cf6f4aa0e89545c6f62bcf77d
Ashwin Hari <ashwin.hari1836@gmail.com>
2019-01-30 18:46:53 +0000

    Added another FIFO test that passes reliably.
    The host repeats for each fifo: write 10 packets and read back 10 packets/verify that they match.
    
    This is different from the previous test in the multiple packets are made to occupy the FIFOs.

--- cl_mcl/software/deploy/fifo_test.c
+++ cl_mcl/software/deploy/fifo_test.c
@@ -15,6 +15,7 @@
 static const int DW_PER_FSB = 4;
 
 void test_fifos();
+void fifo_many();
 void loop_test(int n, void (*f) ());
 
 int main () {
@@ -32,7 +33,7 @@ int main () {
 		return 0;
 	}
 
-	loop_test(50, test_fifos);
+	loop_test(50, fifo_many);
 
 	return 0;
 }
@@ -42,6 +43,7 @@ void loop_test (int n, void (*f) () ) {
 	for (int i = 0; i < n; i++) 
 		f();
 }
+
 void test_fifos () {
 	int *write, *read, num_fsb;
 	num_fsb = 10;
@@ -87,11 +89,71 @@ void test_fifos () {
 		if (pass) {
 			printf("packet %d passed.\n", pkt) ;
 		}
+		else {
+			printf("packet %d failed.\n", pkt);	
+		}
 	}
 	free(write);
 }
 
+int is_equal (int *read, int *write) {
+	for (int i = 0; i < 2; i++) {
+		if (read[i] != write[i])
+			return i;
+	}	
+	return -1;
+}
 
+/* write many packets to each FIFO. and then read back. */ 
+void fifo_many () {
+	int *write, *read, num_fsb;
+	num_fsb = 10;
+	write = calloc(NUM_FIFO * num_fsb * DW_PER_FSB, sizeof(int)); 	
 
+	/* clear fifo interrupts */
+	for (int f = 0; f < NUM_FIFO; f++)
+		clear_int(f);
 
+	for (int f = 0; f < NUM_FIFO; f++) {
+		/* write num_fsb packets to FIFO f */
+		for (int pkt = 0; pkt < num_fsb; pkt++) {
+			for(int j = 0; j < 4; j++) { /* make write packet */
+				write[(f * num_fsb * DW_PER_FSB) + (pkt * DW_PER_FSB) + j] = rand();
+			}
+			bool is_write = deploy_write_fifo(f, write + (f * num_fsb * DW_PER_FSB) + (pkt * DW_PER_FSB)); /* write the packets */
+			if (!is_write)
+				printf("WARNING: write packet %d to FIFO %d was not succesful.\n", pkt, f);
+		}
+
+		usleep(100);
+		
+		/* read num_fsb packets from FIFO f  */
+		bool pass = true;
+		for (int pkt = 0; pkt < num_fsb; pkt++) {
+			read = deploy_read_fifo(f, NULL);
+			if (!read) {
+				printf("No read data for fsb packet %d received from fifo %d.\n", pkt, f);
+				pass = false;
+				break;
+			}
+			int *write_pkt = write + (f * num_fsb * DW_PER_FSB) + (pkt * DW_PER_FSB);
+			int mismatch = is_equal(read, write_pkt);
+			if (mismatch != -1) {
+				printf("Mismatch at fsb packet %d, DW %d from fifo %d. Received %d instead of %x. \n", pkt, mismatch, f, read[mismatch], write_pkt[mismatch]);
+				pass = false;
+				free (read);
+				break;
+			}
+			else // successful
+				free(read);
+		}
+		if (pass) {
+			printf("fifo %d passed.\n", f) ;
+		}
+		else {
+			printf("\n\nFIFO %d FAILED.\n\n\n", f);
+		}
+	}
+	free(write);
+}
 
