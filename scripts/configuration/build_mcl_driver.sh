29c861d86e5a25e3a6c56a5e79d196e2d706702f
Ashwin Hari <ashwin.hari1836@gmail.com>
2019-02-13 21:10:24 +0000

    Added script for building mcl drivers. Successfully tested on a 4x4 AFI.

--- scripts/configuration/build_mcl_driver.sh
+++ scripts/configuration/build_mcl_driver.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 # This script builds and installs the BSG Manycore Link PCIe Driver
+
+cd ../../cl_manycore/software/deploy/driver
+make clean
+make
+sudo rmmod xdma.ko
+sudo insmod bsg_dma_driver.ko slot=0x1d
+dmesg | grep "major number is:" | awk '{system("sudo mknod /dev/bsg_dma_driver c " $NF " 0")}'
+sudo chmod 777 /dev/bsg_dma_driver
+cd ../../../../scripts/configuration
