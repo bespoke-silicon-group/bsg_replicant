540dd879740121a46bca5f86f596faccbf9316a4
Ashwin Hari <ashwin.hari1836@gmail.com>
2019-03-20 01:15:20 +0000

    Fixed incompatibility between host program examples and bitstream, which has the tile group feature.

--- cl_manycore/examples/read_write_test.c
+++ cl_manycore/examples/read_write_test.c
@@ -26,16 +26,16 @@ int main () {
 	
 	/* store data in tile */
 	uint32_t data = 0xABCD;
-	bool write = hb_mc_copy_to_epa(fd, 0, 0, DMEM_BASE >> 2, &data, 1);
+	bool write = hb_mc_copy_to_epa(fd, 0, 1, DMEM_BASE >> 2, &data, 1);
 
 	if (!write) {
-		printf("writing data to tile (0, 0)'s DMEM failed.\n");
+		printf("writing data to tile (0, 1)'s DMEM failed.\n");
 		return 0;
 	}
 
 	/* read back data */
 	uint32_t **buf = (uint32_t **) calloc(1, sizeof(uint32_t *));
-	bool read = hb_mc_copy_from_epa(fd, buf, 0, 0, DMEM_BASE >> 2, 1); 
+	bool read = hb_mc_copy_from_epa(fd, buf, 0, 1, DMEM_BASE >> 2, 1); 
 	printf("completed read.\n");
 	if (read == 1) {
 		printf("read packet: ");
--- cl_manycore/examples/run_manycore_program.c
+++ cl_manycore/examples/run_manycore_program.c
@@ -12,26 +12,26 @@
 #include <bsg_manycore_loader.h>
 #include <bsg_manycore_print.h>
 
-/* This is a skeleton for running a manycore program. It runs the program on Tile (0, 0). */
-
 int main () {
+
+	char *manycore_program = "";
 	
 	uint8_t fd;
-	char *path_to_manycore_program = ""; /* Put path to the riscv file here */
-	if (!hb_mc_init_host(path_to_manycore_program, &fd)) {
+	if (!hb_mc_init_host(&fd)) {
 		printf("failed to initialize host.\n");
 		return 0;
 	}
 
-	uint8_t x = 0, y = 0;
+	uint8_t x = 0, y = 1;
 
-        hb_mc_freeze(fd, 0, 0);
-	
-	hb_mc_load_binary(fd, path_to_manycore_program, &x, &y, 1);
+        hb_mc_freeze(fd, 0, 1);
 
-  	hb_mc_unfreeze(fd, 0, 0);
+	hb_mc_set_tile_group_origin(fd, 0, 1, 0, 1);
+	printf("file to be loaded is %s\n", manycore_program);
+	hb_mc_load_binary(fd, manycore_program, &x, &y, 1);
 
-	/* Assuming that the manycore program sends a packet to the host, this code attempts to read the packet */
+  	hb_mc_unfreeze(fd, 0,1);
+
 	printf("Checking receive packet...\n");
 	usleep(100); /* 100 us */	
 	uint32_t *receive_packet = hb_mc_read_fifo(fd, 1, NULL);
@@ -39,4 +39,5 @@ int main () {
 	hb_mc_print_hex((uint8_t *) receive_packet);
 
 	return 0;
+
 }
