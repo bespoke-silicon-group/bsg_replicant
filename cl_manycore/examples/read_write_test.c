f4fa9aaa9f458a8195afbcd9b7362c778418d0d2
Ashwin Hari <ashwin.hari1836@gmail.com>
2019-03-25 16:47:10 -0700

    Commit 37d3c98 breaks dev branch on F1. This commit amends the examples to use the integer error codes. (#34)

--- cl_manycore/examples/read_write_test.c
+++ cl_manycore/examples/read_write_test.c
@@ -12,13 +12,14 @@
 #include <bsg_manycore_mem.h>
 #include <bsg_manycore_loader.h>
 #include <bsg_manycore_print.h>
+#include <bsg_manycore_errno.h>
 
 int main () {
 	
 	printf("Running the Read/Write test on the Manycore with 4 x 4 dimensions.\n\n");
 
 	uint8_t fd;
-	if (!hb_mc_init_host(&fd)) {
+	if (hb_mc_init_host(&fd) != HB_MC_SUCCESS) {
 		printf("failed to initialize host.\n");
 		return 0;
 	}
@@ -26,18 +27,18 @@ int main () {
 	
 	/* store data in tile */
 	uint32_t data = 0xABCD;
-	bool write = hb_mc_copy_to_epa(fd, 0, 1, DMEM_BASE >> 2, &data, 1);
+	int write = hb_mc_copy_to_epa(fd, 0, 1, DMEM_BASE >> 2, &data, 1);
 
-	if (!write) {
+	if (write != HB_MC_SUCCESS) {
 		printf("writing data to tile (0, 1)'s DMEM failed.\n");
 		return 0;
 	}
 
 	/* read back data */
 	uint32_t **buf = (uint32_t **) calloc(1, sizeof(uint32_t *));
-	bool read = hb_mc_copy_from_epa(fd, buf, 0, 1, DMEM_BASE >> 2, 1); 
+	int read = hb_mc_copy_from_epa(fd, buf, 0, 1, DMEM_BASE >> 2, 1); 
 	printf("completed read.\n");
-	if (read == 1) {
+	if (read == HB_MC_SUCCESS) {
 		printf("read packet: ");
 		hb_mc_print_hex((uint8_t *) buf[0]);
 	}
