09f89391d8009a530bbff03326678feaa1143e0a
Dustin Richmond <dustinar@uw.edu>
2019-04-16 10:41:46 -0700

    Quality of life fixes

--- cl_manycore/regression/spmd/test_bsg_dram_loopback_cache.c
+++ cl_manycore/regression/spmd/test_bsg_dram_loopback_cache.c
@@ -2,9 +2,6 @@
 #include "test_bsg_dram_loopback_cache.h"
 
 int test_loopback () {
-
-	printf("Running the Manycore-Cache-Loopback test on a 4x4.\n\n");
-
 	uint8_t fd;
 	if (hb_mc_init_host(&fd) != HB_MC_SUCCESS) {
 		printf("failed to initialize host.\n");
@@ -27,12 +24,11 @@ int test_loopback () {
 	char* bsg_manycore_dir = BSG_STRINGIFY(BSG_MANYCORE_DIR) "/software/spmd/"
 		"bsg_dram_loopback_cache/main.riscv";
 
-	printf("%s\n", bsg_manycore_dir);
 	hb_mc_load_binary(fd, bsg_manycore_dir, &x, &y, 1);
 
   	hb_mc_unfreeze(fd, 0, 1);
 
-	printf("Checking receive packet...\n");
+	printf("BSG INFO: Checking receive packet...\n");
 	usleep(100); /* 100 us */
 	hb_mc_packet_t manycore_finish;
 	hb_mc_read_fifo(fd, 1, &manycore_finish);	
@@ -45,34 +41,34 @@ int test_loopback () {
 	uint32_t x_dst = hb_mc_request_packet_get_x_dst(&manycore_finish.request);
 	uint32_t y_dst = hb_mc_request_packet_get_y_dst(&manycore_finish.request);
 	uint32_t op = hb_mc_request_packet_get_op(&manycore_finish.request);
-	printf("Manycore finish packet received at Address 0x%x at coordinates (0x%x, 0x%x) from (0x%x, 0x%x). Operation: 0x%x, Data: 0x%x\n", addr, x_dst, y_dst, x_src, y_src, op, data & op_ex);
+	printf("BSG INFO: Manycore finish packet received at Address 0x%x at coordinates (0x%x, 0x%x) from (0x%x, 0x%x). Operation: 0x%x, Data: 0x%x\n", addr, x_dst, y_dst, x_src, y_src, op, data & op_ex);
 	if (addr == 0x3ab4)
 		return HB_MC_SUCCESS;
 	else
 		return HB_MC_FAIL;
- }
+}
 
 
 #ifdef COSIM
-	void test_main(uint32_t *exit_code) {	
-		printf("Regression Test on COSIMULATION:\n\n");
-		int rc = test_loopback();
-		*exit_code = rc;
-		if (rc == HB_MC_SUCCESS)
-			printf("TEST PASSED\n");
-		else
-			printf("TEST FAILED!!!\n");
-		return;
-	}
+void test_main(uint32_t *exit_code) {	
+	printf("BSG INFO: test_bsg_dram_loopback_cache Regression Test (COSIMULATION)\n");
+	int rc = test_loopback();
+	*exit_code = rc;
+	if (rc == HB_MC_SUCCESS)
+		printf("BSG REGRESSION TEST PASSED\n");
+	else
+		printf("BSG REGRESSION TEST FAILED\n");
+	return;
+}
 #else
-	int main() {
-		printf("Regression Test on F1:\n\n");
-		int rc = test_loopback();
-		if (rc == HB_MC_SUCCESS)
-			printf("TEST PASSED\n");
-		else
-			printf("TEST FAILED!!!\n");
-		return rc;
-	}
+int main() {
+	printf("BSG INFO: test_bsg_dram_loopback_cache Regression Test (F1)\n");
+	int rc = test_loopback();
+	if (rc == HB_MC_SUCCESS)
+		printf("BSG REGRESSION TEST PASSED\n");
+	else
+		printf("BSG REGRESSION TEST FAILED\n");
+	return rc;
+}
 #endif
 
