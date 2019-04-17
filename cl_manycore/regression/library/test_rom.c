aa7fbc42d8d52cac8ae1e33f12a91408db038881
Sasha Krassovsky <warplane@hotmail.com>
2019-04-17 14:45:25 -0700

    Test rom fix (#123)
    * fixed test_rom to read specific values

--- cl_manycore/regression/library/test_rom.c
+++ cl_manycore/regression/library/test_rom.c
@@ -1,4 +1,4 @@
-#include "test_rom.h"
+ #include "test_rom.h"
 
 int print_rom(uint8_t fd, int idx, int num) {
 	hb_mc_response_packet_t buf[num];
@@ -16,6 +16,20 @@ int print_rom(uint8_t fd, int idx, int num) {
 	}
 }
 
+int read_rom(uint8_t fd, int idx, int num, /* out */ uint32_t *result) {
+    	hb_mc_response_packet_t buf[num];
+	int read = hb_mc_copy_from_epa(fd, &buf[0], 0, 0, idx, num);
+	if (read == HB_MC_SUCCESS) {
+		for (int i=0; i<num; i++) {
+			result[i] = hb_mc_response_packet_get_data(&buf[i]);
+			bsg_pr_test_info("Read rom data from address 0x%x: 0x%x\n", idx + i, result); 
+		}
+		return HB_MC_SUCCESS;
+	}
+        bsg_pr_test_info("Read from ROM failed.\n");
+        return HB_MC_FAIL;
+}
+
 int test_rom () {
 	uint8_t fd = 0;
 	hb_mc_init_host(&fd);
@@ -24,7 +38,26 @@ int test_rom () {
 	uint32_t recv_vacancy = hb_mc_get_recv_vacancy(fd);
 	bsg_pr_test_info("Recv vacancy is: %x\n", recv_vacancy);
 	bsg_pr_test_info("Readback ROM from tile (%d, %d)\n", 0, 0);
-	return print_rom(fd, 0, 12);
+
+        // In order, the elements of the array are:
+        // NETWORK_DIM_X, NETWORK_DIM_Y, HOST_INTERFACE_COORD_X, HOST_INTERFACE_COORD_Y
+        const char *desc[4] = { "Network X Dimension", "Network Y Dimension", "Host X Coordinate", "Host Y Coordinate" };
+        uint32_t expected[4] = { CL_MANYCORE_DIM_X, CL_MANYCORE_DIM_Y, 0, CL_MANYCORE_DIM_Y - 1 };
+        uint32_t actual[4] = {};
+        
+        if(read_rom(fd, 0, 4, actual) == HB_MC_FAIL) {
+            return HB_MC_FAIL;
+        }
+        bsg_pr_test_info("Comparing results:\n");
+        for(int i = 0; i < 4; i++) {
+            bsg_pr_test_info("%s: Expected = %d, Actual = %d", desc[i], expected[i], actual[i]);
+            if(expected[i] != actual[i]) {
+                printf("\033[31m Failed \033[0m\n");
+                return HB_MC_FAIL;
+            }
+            printf("\033[033m Succeeded \033[0m\n");
+        }
+        return HB_MC_SUCCESS;
 }
 
 #ifdef COSIM
