4e916cbaeadb4439cc9de704512d43bb89a005a1
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-04-23 03:54:23 +0000

    Added test_bsg_cuda_lite_runtime_empty regression test. This test is designed to test read and write into manycore DRAM.

---
+++ cl_manycore/regression/spmd/test_bsg_cuda_lite_runtime_empty.h
@@ -0,0 +1,15 @@
+#ifndef TEST_BSG_CUDA_LITE_RUNTIME_EMPTY_H
+#define TEST_BSG_CUDA_LITE_RUNTIME_EMPTY_H
+
+
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "spmd_tests.h"
+
+
+#endif
