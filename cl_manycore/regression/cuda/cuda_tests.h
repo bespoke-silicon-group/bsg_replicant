96a39a17e4b2c32b98d22f622082da6cfdfc17a2
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-04-29 22:58:40 +0000

    1. Separated SPMD and CUDA regression tests. 2. Added regression/cuda path with the list of cuda tests. 3. Only compatible with dev_cuda_tile_group_refactored branch on bsg_manycore.

--- cl_manycore/regression/cuda/cuda_tests.h
+++ cl_manycore/regression/cuda/cuda_tests.h
@@ -1,22 +1,22 @@
-#ifndef __SPMD_TESTS_H
-#define __SPMD_TESTS_H
+#ifndef __CUDA_TESTS_H
+#define __CUDA_TESTS_H
 
 #ifdef COSIM
 
 #include <utils/sh_dpi_tasks.h>
 #include "fpga_pci_sv.h"
 #include "bsg_manycore_driver.h"
-#include "bsg_manycore_tile.h"
 #include "bsg_manycore_mem.h"
 #include "bsg_manycore_loader.h"
 #include "bsg_manycore_errno.h"	
+#include "bsg_manycore_cuda.h"
 #else // !COSIM
 
 #include <bsg_manycore_driver.h>
-#include <bsg_manycore_tile.h>
 #include <bsg_manycore_mem.h>
 #include <bsg_manycore_loader.h>
 #include <bsg_manycore_errno.h>
+#include <bsg_manycore_cuda.h>
 #endif // #ifdef COSIM
 
 #include "../cl_manycore_regression.h"
@@ -24,4 +24,4 @@
 #define __BSG_STRINGIFY(arg) #arg
 #define BSG_STRINGIFY(arg) __BSG_STRINGIFY(arg)
 
-#endif // __SPMD_TESTS_H
+#endif // __CUDA_TESTS_H
