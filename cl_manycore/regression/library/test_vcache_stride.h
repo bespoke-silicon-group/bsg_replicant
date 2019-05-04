59fe0f3de49e2a6fb4c65a7c528e16051ddd427e
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-04 00:53:24 +0000

    Added a Victim cache read/write regression test.

---
+++ cl_manycore/regression/library/test_vcache_stride.h
@@ -0,0 +1,12 @@
+#ifndef __TEST_VCACHE_STRIDE_H
+#define __TEST_VCACHE_STRIDE_H
+
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include "library_tests.h"
+
+#endif // __TEST_VCACHE_STRIDE_H
