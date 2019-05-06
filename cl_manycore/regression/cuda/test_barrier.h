6481b2ae151a938327af5df11c02ce3d50a22078
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-06 20:33:45 +0000

    Added a CUDA barrier regression test, kernels only perform a barrier within the tile group.

---
+++ cl_manycore/regression/cuda/test_barrier.h
@@ -0,0 +1,15 @@
+#ifndef TEST_BARRIER_H
+#define TEST_BARRIER_H
+
+
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "cuda_tests.h"
+
+
+#endif
