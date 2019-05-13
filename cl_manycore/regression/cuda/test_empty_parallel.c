c84d3b6f9ab5db29c87631dcb23436d535f54cd8
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-13 00:00:45 +0000

    1. Renamed "grid" to "mesh" in CUDA API. 2. Modified regression tests accordingly.

--- cl_manycore/regression/cuda/test_empty_parallel.c
+++ cl_manycore/regression/cuda/test_empty_parallel.c
@@ -1,21 +1,21 @@
 #include "test_empty_parallel.h"
 
 /*!
- * Runs 5 empty kernels in 2x2 tile groups on a 4x4 grid. 
+ * Runs 5 empty kernels in 2x2 tile groups on a 4x4 mesh. 
  * This tests uses the software/spmd/bsg_cuda_lite_runtime/empty_parallel/ Manycore binary in the dev_cuda_tile_group_refactored branch of the BSG Manycore github repository.  
 */
 int kernel_empty_parallel () {
 	fprintf(stderr, "Running the CUDA Empyt Kernel on a 2x2 tile group.\n\n");
 
 	device_t device;
-	uint8_t grid_dim_x = 4;
-	uint8_t grid_dim_y = 4;
-	uint8_t grid_origin_x = 0;
-	uint8_t grid_origin_y = 1;
+	uint8_t mesh_dim_x = 4;
+	uint8_t mesh_dim_y = 4;
+	uint8_t mesh_origin_x = 0;
+	uint8_t mesh_origin_y = 1;
 	eva_id_t eva_id = 0;
 	char* elf = BSG_STRINGIFY(BSG_MANYCORE_DIR) "/software/spmd/bsg_cuda_lite_runtime" "/empty_parallel/main.riscv";
 
-	hb_mc_device_init(&device, eva_id, elf, grid_dim_x, grid_dim_y, grid_origin_x, grid_origin_y);
+	hb_mc_device_init(&device, eva_id, elf, mesh_dim_x, mesh_dim_y, mesh_origin_x, mesh_origin_y);
 
 	uint8_t tg_dim_x = 2;
 	uint8_t tg_dim_y = 2;
