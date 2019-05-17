fc7671123dba9269318806317ddc6257c6c5ae7c
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-17 01:47:39 +0000

    1. Added a shared memroy cuda regression test. 2. Modified cuda Makefile

---
+++ cl_manycore/regression/cuda/test_shared_mem.h
@@ -0,0 +1,15 @@
+#ifndef TEST_SHARED_MEM_H
+#define TEST_SHARED_MEM_H
+
+
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "cuda_tests.h"
+
+
+#endif
