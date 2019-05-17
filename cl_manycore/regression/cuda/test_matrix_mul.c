9004e5d9ee5764a228665bcef788360c165e9bd4
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-17 00:48:47 +0000

    Modified the matrix multiplication regression test, added commentary.

--- cl_manycore/regression/cuda/test_matrix_mul.c
+++ cl_manycore/regression/cuda/test_matrix_mul.c
@@ -1,21 +1,24 @@
 #include "test_matrix_mul.h"
 
 /*!
- * Runs the matrix multiplication on a grid of 4 2x2 tile groups. 
+ * Runs the matrix multiplication on a grid of 2x2 tile groups. A[M][N] * B[N][P] --> C[M][P]
+ * Grid dimensions are determines by how much of a load we want for each tile group (block_size_y/x)
  * This tests uses the software/spmd/bsg_cuda_lite_runtime/matrix_mul/ Manycore binary in the dev_cuda_v4 branch of the BSG Manycore bitbucket repository.  
 */
 
 
 
-
-void matrix_mult (uint32_t *A, uint32_t *B, uint32_t *C, int n) { 
-	for (int y = 0; y < n; y ++) { 
-		for (int x = 0; x < n; x ++) { 
+/*! 
+ * Matrix multiplication code on the host side to compare the results
+ */
+void matrix_mult (uint32_t *A, uint32_t *B, uint32_t *C, int M, int N, int P) { 
+	for (int y = 0; y < M; y ++) { 
+		for (int x = 0; x < P; x ++) { 
 			int res = 0;
-			for (int k = 0; k < n; k++) { 
-				res += A[y * n + k] * B[k * n + x];
+			for (int k = 0; k < N; k++) { 
+				res += A[y * N + k] * B[k * P + x];
 			}
-			C[y * n + x] = res;
+			C[y * P + x] = res;
 		}
 	}
 	return;
@@ -26,6 +29,14 @@ void matrix_mult (uint32_t *A, uint32_t *B, uint32_t *C, int n) {
 int kernel_matrix_mul () {
 	fprintf(stderr, "Running the CUDA Matrix Multiplication Kernel on a grid of 4 2x2 tile groups.\n\n");
 
+	srand(time); 
+
+
+	/*****************************************************************************************************************
+	* Define the dimension of tile pool.
+	* Define path to binary.
+	* Initialize device, load binary and unfreeze tiles.
+	******************************************************************************************************************/
 	device_t device;
 	uint8_t mesh_dim_x = 4;
 	uint8_t mesh_dim_y = 4;
@@ -36,56 +47,108 @@ int kernel_matrix_mul () {
 
 	hb_mc_device_init(&device, eva_id, elf, mesh_dim_x, mesh_dim_y, mesh_origin_x, mesh_origin_y);
 
-	uint32_t n = 16;
-	uint32_t size_buffer = n * n; 
-	eva_t A_device, B_device, C_device; 
-	hb_mc_device_malloc(&device, size_buffer * sizeof(uint32_t), &A_device); /* allocate A on the device */
-	hb_mc_device_malloc(&device, size_buffer * sizeof(uint32_t), &B_device); /* allocate B on the device */
-	hb_mc_device_malloc(&device, size_buffer * sizeof(uint32_t), &C_device); /* allocate C on the device */
 
-	uint32_t A_host[size_buffer]; /* allocate A on the host */ 
-	uint32_t B_host[size_buffer]; /* allocate B on the host */
-	srand(0);
-	for (int i = 0; i < size_buffer; i++) { /* fill A and B with arbitrary data */
-		A_host[i] = rand() % ((1 << 16) - 1); /* avoid overflow */
-		B_host[i] = rand() % ((1 << 16) - 1); 
+
+
+	/*****************************************************************************************************************
+	* Allocate memory on the device for A, B and C.
+	******************************************************************************************************************/
+	uint32_t M = 32;
+	uint32_t N = 64;
+	uint32_t P = 16;
+
+	eva_t A_device, B_device, C_device; 
+	hb_mc_device_malloc(&device, M * N * sizeof(uint32_t), &A_device); /* allocate A[M][N] on the device */
+	hb_mc_device_malloc(&device, N * P * sizeof(uint32_t), &B_device); /* allocate B[N][P] on the device */
+	hb_mc_device_malloc(&device, M * P * sizeof(uint32_t), &C_device); /* allocate C[M][P] on the device */
+
+
+
+	/*****************************************************************************************************************
+	* Allocate memory on the host for A & B and initialize with random values.
+	******************************************************************************************************************/
+	uint32_t A_host[M * N]; /* allocate A[M][N] on the host */ 
+	uint32_t B_host[N * P]; /* allocate B[N][P] on the host */
+	for (int i = 0; i < M * N; i++) { /* fill A with arbitrary data */
+		A_host[i] = rand() & 0xFFFF;
+	}
+	for (int i = 0; i < N * P; i++) { /* fill B with arbitrary data */
+		B_host[i] = rand() & 0xFFFF;
 	}
 
+
+
+
+	/*****************************************************************************************************************
+	* Copy A & B from host onto device DRAM.
+	******************************************************************************************************************/
 	void *dst = (void *) ((intptr_t) A_device);
 	void *src = (void *) &A_host[0];
-	hb_mc_device_memcpy (&device, dst, src, size_buffer * sizeof(uint32_t), hb_mc_memcpy_to_device); /* Copy A1 to the device  */	
+	hb_mc_device_memcpy (&device, dst, src, (M * N) * sizeof(uint32_t), hb_mc_memcpy_to_device); /* Copy A1 to the device  */	
 	dst = (void *) ((intptr_t) B_device);
 	src = (void *) &B_host[0];
-	hb_mc_device_memcpy (&device, dst, src, size_buffer * sizeof(uint32_t), hb_mc_memcpy_to_device); /* Copy B1 to the device */ 
+	hb_mc_device_memcpy (&device, dst, src, (N * P) * sizeof(uint32_t), hb_mc_memcpy_to_device); /* Copy B1 to the device */ 
 
-	uint8_t grid_dim_x = 2;
-	uint8_t grid_dim_y = 2;
+
+
+	/*****************************************************************************************************************
+	* Define block_size_x/y: amount of work for each tile group
+	* Define tg_dim_x/y: number of tiles in each tile group
+	* Calculate grid_dim_x/y: number of tile groups needed based on block_size_x/y
+	******************************************************************************************************************/
+	uint32_t block_size_x = 4;
+	uint32_t block_size_y = 4;
+
 	uint8_t tg_dim_x = 2;
 	uint8_t tg_dim_y = 2;
 
-	int argv[4] = {A_device, B_device, C_device, n};
+	uint32_t grid_dim_x = P / block_size_x;
+	uint32_t grid_dim_y = M / block_size_y;
 
-	hb_mc_grid_init (&device, grid_dim_x, grid_dim_y, tg_dim_x, tg_dim_y, "kernel_matrix_mul", 4, argv);
 
+	/*****************************************************************************************************************
+	* Prepare list of input arguments for kernel.
+	******************************************************************************************************************/
+	int argv[8] = {A_device, B_device, C_device, M, N, P, block_size_y, block_size_x};
+
+	/*****************************************************************************************************************
+	* Enquque grid of tile groups, pass in grid and tile group dimensions, kernel name, number and list of input arguments
+	******************************************************************************************************************/
+	hb_mc_grid_init (&device, grid_dim_x, grid_dim_y, tg_dim_x, tg_dim_y, "kernel_matrix_mul", 8, argv);
+
+	/*****************************************************************************************************************
+	* Launch and execute all tile groups on device and wait for all to finish. 
+	******************************************************************************************************************/
 	hb_mc_device_tile_groups_execute(&device);
 	
 
-	uint32_t C_result[size_buffer];
+
+	/*****************************************************************************************************************
+	* Copy result matrix back from device DRAM into host memory. 
+	******************************************************************************************************************/
+	uint32_t C_result[M * P];
 	src = (void *) ((intptr_t) C_device);
 	dst = (void *) &C_result[0];
-	hb_mc_device_memcpy (&device, (void *) dst, src, size_buffer * sizeof(uint32_t), hb_mc_memcpy_to_host); /* copy C to the host */
-
-	hb_mc_device_finish(&device); /* freeze the tiles and memory manager cleanup */
+	hb_mc_device_memcpy (&device, (void *) dst, src, (M * P) * sizeof(uint32_t), hb_mc_memcpy_to_host); /* copy C to the host */
 
 
-	/* calculate expected results. */
-	uint32_t C_expected[size_buffer]; 
-	matrix_mult (A_host, B_host, C_expected, n); 
+
+	/*****************************************************************************************************************
+	* Freeze the tiles and memory manager cleanup. 
+	******************************************************************************************************************/
+	hb_mc_device_finish(&device); 
+
+
+	/*****************************************************************************************************************
+	* Calculate the expected result matrix using host code and compare the results. 
+	******************************************************************************************************************/
+	uint32_t C_expected[M * P]; 
+	matrix_mult (A_host, B_host, C_expected, M, N, P); 
 
 
 
 	int mismatch = 0; 
-	for (int i = 0; i < size_buffer; i++) {
+	for (int i = 0; i < M * P; i++) {
 		if ( C_expected[i] != C_result[i]) {
 			mismatch = 1;
 			break; 
@@ -93,17 +156,17 @@ int kernel_matrix_mul () {
 	}
 
 	fprintf(stderr, "Expected Result:\n");
-	for (int y = 0; y < n; y++) { 
-		for (int x = 0; x < n; x++) {
-			fprintf(stderr, "%d\t", C_expected[y * n + x]);
+	for (int y = 0; y < M; y++) { 
+		for (int x = 0; x < P; x++) {
+			fprintf(stderr, "%d\t", C_expected[y * P + x]);
 		}
 		fprintf(stderr, "\n");
 	}
 		
 	fprintf(stderr, "Manycore Result:\n");
-	for (int y = 0; y < n; y++) { 
-		for (int x = 0; x < n; x++) {
-			fprintf(stderr, "%d\t", C_result[y * n + x]);
+	for (int y = 0; y < M; y++) { 
+		for (int x = 0; x < P; x++) {
+			fprintf(stderr, "%d\t", C_result[y * P + x]);
 		}
 		fprintf(stderr, "\n");
 	}
