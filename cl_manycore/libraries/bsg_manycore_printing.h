74460ae8474a633bc397c59ab23f6768e7b99668
Max Ruttenberg <mrutt@cs.washington.edu>
2019-05-21 14:44:09 -0700

    implements the sim time printing for realz

--- cl_manycore/libraries/bsg_manycore_printing.h
+++ cl_manycore/libraries/bsg_manycore_printing.h
@@ -13,22 +13,21 @@
 extern "C" {
 #endif // #if defined(__cplusplus)
 
-#if defined(COSIM)
-#define BSG_PRINT_PREFIX_DEBUG "DEBUG @(%llu/%llu):   "
+/* #if defined(COSIM) */
+/* #define BSG_PRINT_PREFIX_DEBUG "DEBUG @(%llu/%llu):   " */
+/* #else // !defined(COSIM) */
+/* #define BSG_PRINT_PREFIX_DEBUG "DEBUG @(%llu):   " */
+/* #endif */
+
+#define BSG_PRINT_PREFIX_DEBUG "DEBUG:   "
 #define BSG_PRINT_PREFIX_ERROR "ERROR:   "
 #define BSG_PRINT_PREFIX_WARN  "WARNING: "
 #define BSG_PRINT_PREFIX_INFO  "INFO:    "
-#else // !defined(COSIM)
-#define BSG_PRINT_PREFIX_DEBUG "DEBUG @(%llu):   "
-#define BSG_PRINT_PREFIX_ERROR "ERROR:   "
-#define BSG_PRINT_PREFIX_WARN  "WARNING: "
-#define BSG_PRINT_PREFIX_INFO  "INFO:    "
-#endif
 
 #define BSG_PRINT_STREAM_DEBUG stderr
 #define BSG_PRINT_STREAM_ERROR stderr
 #define BSG_PRINT_STREAM_WARN  stderr
-#define BSG_PRINT_STREAM_INFO  stderr               
+#define BSG_PRINT_STREAM_INFO  stderr
 
 #ifdef COSIM
 extern void sv_bsg_realtime(uint64_t*);
@@ -48,23 +47,30 @@ static inline uint64_t bsg_utc(){
 
 __attribute__((format(printf, 2, 3)))
 int bsg_pr_prefix(const char *prefix, const char *fmt, ...);
-        
-#if defined(DEBUG) && defined(COSIM)
-#define bsg_pr_dbg(fmt, ...)                    \
-        bsg_pr_prefix(BSG_PRINT_PREFIX_DEBUG, fmt, bsg_realtime(), bsg_utc(), ##__VA_ARGS__)
-#elif defined(DEBUG)
-#define bsg_pr_dbg(fmt, ...)                    \
-        bsg_pr_prefix(BSG_PRINT_PREFIX_DEBUG, fmt, bsg_utc(), ##__VA_ARGS__)
-#else 
+
+/* #if defined(DEBUG) && defined(COSIM) */
+/* #define bsg_pr_dbg(fmt, ...)                    \ */
+/*         bsg_pr_prefix(BSG_PRINT_PREFIX_DEBUG, fmt, bsg_realtime(), bsg_utc(), ##__VA_ARGS__) */
+/* #elif defined(DEBUG) */
+/* #define bsg_pr_dbg(fmt, ...)                    \ */
+/*         bsg_pr_prefix(BSG_PRINT_PREFIX_DEBUG, fmt, bsg_utc(), ##__VA_ARGS__) */
+/* #else  */
+/* #define bsg_pr_dbg(...) */
+/* #endif */
+
+#if defined(DEBUG)
+#define bsg_pr_dbg(fmt, ...)			\
+	bsg_pr_prefix(BSG_PRINT_PREFIX_DEBUG, fmt, ##__VA_ARGS__)
+#else
 #define bsg_pr_dbg(...)
 #endif
 
 #define bsg_pr_err(fmt, ...)                                            \
         bsg_pr_prefix(BSG_PRINT_PREFIX_ERROR, fmt, ##__VA_ARGS__)
-        
+
 #define bsg_pr_warn(fmt, ...)                                           \
         bsg_pr_prefix(BSG_PRINT_PREFIX_WARN, fmt, ##__VA_ARGS__)
-        
+
 #define bsg_pr_info(fmt, ...)                                           \
         bsg_pr_prefix(BSG_PRINT_PREFIX_INFO, fmt, ##__VA_ARGS__)
 
@@ -72,5 +78,5 @@ int bsg_pr_prefix(const char *prefix, const char *fmt, ...);
 #if defined(__cplusplus)
 }
 #endif // #if defined(__cplusplus)
-        
+
 #endif
