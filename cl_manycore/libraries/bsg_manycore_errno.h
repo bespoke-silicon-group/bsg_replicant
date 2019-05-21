8799038fc1956e3588f98df3f571b2e31e615b66
Max Ruttenberg <mrutt@cs.washington.edu>
2019-05-21 13:39:48 -0700

    added a 'no-implementation' error code

--- cl_manycore/libraries/bsg_manycore_errno.h
+++ cl_manycore/libraries/bsg_manycore_errno.h
@@ -16,17 +16,19 @@ extern "C" {
 #define HB_MC_INVALID       (-4)
 #define HB_MC_NOMEM         (-5)
 #define HB_MC_INITIALIZED_TWICE (-6)
+#define HB_MC_NOIMPL (-7)
 
 static inline const char * hb_mc_strerror(int err)
 {
         static const char *strtab [] = {
-                [-HB_MC_SUCCESS] = "Success",
-                [-HB_MC_FAIL]    = "Failure",
-                [-HB_MC_TIMEOUT] = "Timeout",
-		[-HB_MC_UNINITIALIZED] = "Manycore not initialized",
-		[-HB_MC_INVALID] = "Invalid input",
-		[-HB_MC_NOMEM]   = "Out of memory",
+                [-HB_MC_SUCCESS]           = "Success",
+                [-HB_MC_FAIL]              = "Failure",
+                [-HB_MC_TIMEOUT]           = "Timeout",
+		[-HB_MC_UNINITIALIZED]     = "Manycore not initialized",
+		[-HB_MC_INVALID]           = "Invalid input",
+		[-HB_MC_NOMEM]             = "Out of memory",
 		[-HB_MC_INITIALIZED_TWICE] = "Non-zeroed input",
+		[-HB_MC_NOIMPL]            = "Not implemented",
         };
         return strtab[-err];
 }
