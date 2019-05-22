7eb342ffd0f4205347ac81aa7ed0b1cfa90f9589
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-05-22 01:24:35 +0000

    Added a cuda dram load store regression test. Tiles in the chip declare an array on the dram, fill it and kick the pointer to it back to host. Host then picks up the array and compares.

---
+++ cl_manycore/regression/cuda/test_dram.h
@@ -0,0 +1,15 @@
+#ifndef TEST_DRAM_H
+#define TEST_DRAM_H
+
+
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "cuda_tests.h"
+
+
+#endif
