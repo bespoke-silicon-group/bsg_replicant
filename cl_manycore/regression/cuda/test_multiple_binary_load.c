ca36bb8b0f1a22bb8a6f12a1f6152fb8d4094c34
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-06-20 18:02:08 +0000

    Added a new cuda regression test to initialize and run kernel and finish device twice.

---
+++ cl_manycore/regression/cuda/test_multiple_binary_load.c
@@ -0,0 +1,178 @@
+#include "test_multiple_binary_load.h"
+
+
+#define TEST_NAME "test_multiple_binary_load"
+#define ALLOC_NAME "default_allocator"
+
+/*!
+ * Runs an empty kernel on a 2x2 tile group twice, with devince_finish and device_init in between
+ * This tests uses the software/spmd/bsg_cuda_lite_runtime/multiple_binary_load/ Manycore binary in the BSG Manycore github repository.  
+*/
+
+int kernel_multiple_binary_load () {
+	bsg_pr_test_info("Running the CUDA Init Finish Kernel on a 2x2 tile group.\n\n");
+	int rc;
+
+	bsg_pr_test_info("PART 1 -- Kernel: empty -- Tile gourp: 2x2.\n");
+	/*****************************************************************************************************************
+	* Define the dimension of tile pool.
+	* Define path to binary.
+	* Initialize device, load binary and unfreeze tiles.
+	******************************************************************************************************************/
+	hb_mc_device_t device;
+	hb_mc_dimension_t mesh_dim = {.x = 4, .y = 4};
+	rc = hb_mc_device_init(&device, TEST_NAME, 0,  mesh_dim);
+	if (rc != HB_MC_SUCCESS) { 
+		bsg_pr_err("failed to initialize device.\n");
+		return rc;
+	}
+
+
+	char* elf = BSG_STRINGIFY(BSG_MANYCORE_DIR) "/software/spmd/bsg_cuda_lite_runtime" "/multiple_binary_load/main.riscv";
+	rc = hb_mc_device_program_init(&device, elf, ALLOC_NAME, 0);
+	if (rc != HB_MC_SUCCESS) { 
+		bsg_pr_err("failed to initialize program.\n");
+		return rc;
+	}
+
+
+	/*****************************************************************************************************************
+	* Define grid_dim_x/y: total number of tile groups
+	* Define tg_dim_x/y: number of tiles in each tile group
+	* Calculate grid_dim_x/y: number of tile groups needed based on block_size_x/y
+	******************************************************************************************************************/
+	hb_mc_dimension_t grid_dim = {.x = 1, .y = 1};
+	hb_mc_dimension_t tg_dim = {.x = 2, .y = 2};
+
+
+	/*****************************************************************************************************************
+	* Prepare list of input arguments for kernel.
+	******************************************************************************************************************/
+	int argv[1];
+
+
+	/*****************************************************************************************************************
+	* Enquque grid of tile groups, pass in grid and tile group dimensions, kernel name, number and list of input arguments
+	******************************************************************************************************************/
+	rc = hb_mc_grid_init (&device, grid_dim, tg_dim, "kernel_empty", 0, argv);
+	if (rc != HB_MC_SUCCESS) { 
+		bsg_pr_err("failed to initialize grid.\n");
+		return rc;
+	}
+	
+
+	/*****************************************************************************************************************
+	* Launch and execute all tile groups on device and wait for all to finish. 
+	******************************************************************************************************************/
+	rc = hb_mc_device_tile_groups_execute(&device);
+	if (rc != HB_MC_SUCCESS) { 
+		bsg_pr_err("failed to execute tile groups.\n");
+		return rc;
+	}
+	
+
+	/*****************************************************************************************************************
+	* Freeze the tiles and memory manager cleanup. 
+	******************************************************************************************************************/
+	rc = hb_mc_device_finish(&device); /* freeze the tiles and memory manager cleanup */
+	if (rc != HB_MC_SUCCESS) { 
+		bsg_pr_err("failed to de-initialize device.\n");
+		return rc;
+	}
+	bsg_pr_test_info(BSG_GREEN("Part 1 Passed.\n"));
+
+
+
+	bsg_pr_test_info("PART 2 -- Kernel: empty -- Tile gourp: 2x2.\n");
+	/*****************************************************************************************************************
+	* Define the dimension of tile pool.
+	* Define path to binary.
+	* Initialize device, load binary and unfreeze tiles.
+	******************************************************************************************************************/
+	mesh_dim = hb_mc_dimension (4, 4);
+	rc = hb_mc_device_init(&device, TEST_NAME, 0,  mesh_dim);
+	if (rc != HB_MC_SUCCESS) { 
+		bsg_pr_err("failed to initialize device.\n");
+		return rc;
+	}
+
+
+	elf = BSG_STRINGIFY(BSG_MANYCORE_DIR) "/software/spmd/bsg_cuda_lite_runtime" "/multiple_binary_load/main.riscv";
+	rc = hb_mc_device_program_init(&device, elf, ALLOC_NAME, 0);
+	if (rc != HB_MC_SUCCESS) { 
+		bsg_pr_err("failed to initialize program.\n");
+		return rc;
+	}
+
+
+	/*****************************************************************************************************************
+	* Define grid_dim_x/y: total number of tile groups
+	* Define tg_dim_x/y: number of tiles in each tile group
+	* Calculate grid_dim_x/y: number of tile groups needed based on block_size_x/y
+	******************************************************************************************************************/
+	grid_dim = hb_mc_dimension (1, 1);
+	tg_dim = hb_mc_dimension (2, 2);
+
+
+	/*****************************************************************************************************************
+	* Prepare list of input arguments for kernel.
+	******************************************************************************************************************/
+
+
+	/*****************************************************************************************************************
+	* Enquque grid of tile groups, pass in grid and tile group dimensions, kernel name, number and list of input arguments
+	******************************************************************************************************************/
+	rc = hb_mc_grid_init (&device, grid_dim, tg_dim, "kernel_empty", 0, argv);
+	if (rc != HB_MC_SUCCESS) { 
+		bsg_pr_err("failed to initialize grid.\n");
+		return rc;
+	}
+	
+
+	/*****************************************************************************************************************
+	* Launch and execute all tile groups on device and wait for all to finish. 
+	******************************************************************************************************************/
+	rc = hb_mc_device_tile_groups_execute(&device);
+	if (rc != HB_MC_SUCCESS) { 
+		bsg_pr_err("failed to execute tile groups.\n");
+		return rc;
+	}
+	
+
+	/*****************************************************************************************************************
+	* Freeze the tiles and memory manager cleanup. 
+	******************************************************************************************************************/
+	rc = hb_mc_device_finish(&device); /* freeze the tiles and memory manager cleanup */
+	if (rc != HB_MC_SUCCESS) { 
+		bsg_pr_err("failed to de-initialize device.\n");
+		return rc;
+	}
+	bsg_pr_test_info(BSG_GREEN("Part 2 Passed.\n"));
+
+
+
+	return HB_MC_SUCCESS;
+}
+
+#ifdef COSIM
+void test_main(uint32_t *exit_code) {	
+#ifdef VCS
+	svScope scope;
+	scope = svGetScopeFromName("tb");
+	svSetScope(scope);
+#endif
+	bsg_pr_test_info("test_multiple_binary_load Regression Test (COSIMULATION)\n");
+	int rc = kernel_multiple_binary_load();
+	*exit_code = rc;
+	bsg_pr_test_pass_fail(rc == HB_MC_SUCCESS);
+	return;
+}
+#else
+int main() {
+	bsg_pr_test_info("test_multiple_binary_load Regression Test (F1)\n");
+	int rc = kernel_multiple_binary_load();
+	bsg_pr_test_pass_fail(rc == HB_MC_SUCCESS);
+	return rc;
+}
+#endif
+
---
+++ cl_manycore/regression/cuda/test_multiple_binary_load.h
@@ -0,0 +1,15 @@
+#ifndef TEST_MULTIPLE_BINARY_LOAD_H
+#define TEST_MULTIPLE_BINARY_LOAD_H
+
+
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "cuda_tests.h"
+
+
+#endif
