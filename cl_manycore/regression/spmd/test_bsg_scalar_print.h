934a73bded36262884ceb130447ff41885440d4b
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-07-19 15:36:20 -0700

    Added spmd regression test for testing scalar printing functionality

---
+++ cl_manycore/regression/spmd/test_bsg_scalar_print.h
@@ -0,0 +1,12 @@
+#ifndef __TEST_BSG_SCALAR_PRINT_H
+#define __TEST_BSG_SCALAR_PRINT_H
+
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+#include "spmd_tests.h"
+
+#endif // __TEST_SCALAR_PRINT_H
