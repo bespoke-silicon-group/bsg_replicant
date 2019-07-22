84f4218fb159600f5421ee4633eb8fecdc91e510
Dustin Richmond <dustinar@uw.edu>
2019-07-22 11:53:39 -0700

    Fixed remaining CUDA Tests

--- cl_manycore/regression/cuda/test_float_vec_add.c
+++ cl_manycore/regression/cuda/test_float_vec_add.c
@@ -200,7 +200,15 @@ int kernel_float_vec_add () {
 }
 
 #ifdef COSIM
-void test_main(uint32_t *exit_code) {	
+void cosim_main(uint32_t *exit_code, char * args) {
+        // We aren't passed command line arguments directly so we parse them
+        // from *args. args is a string from VCS - to pass a string of arguments
+        // to args, pass c_args to VCS as follows: +c_args="<space separated
+        // list of args>"
+        int argc = get_argc(args);
+        char *argv[argc];
+        get_argv(args, argc, argv);
+
 #ifdef VCS
 	svScope scope;
 	scope = svGetScopeFromName("tb");
