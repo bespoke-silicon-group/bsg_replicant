51f5f5e1819ea40cd4a28026adc2e53cb0ca55a7
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-07-23 21:28:21 -0700

    Added a cuda max_pool2d regression test

---
+++ cl_manycore/regression/cuda/test_max_pool2d.h
@@ -0,0 +1,15 @@
+#ifndef TEST_MAX_POOL2D_H
+#define TEST_MAX_POOL2D_H
+
+
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "cuda_tests.h"
+
+
+#endif
