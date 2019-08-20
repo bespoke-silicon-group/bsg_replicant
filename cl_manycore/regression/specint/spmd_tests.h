34ccb1b6edcbde80d17b2af7657c3c432e448258
Bandhav Veluri <bandhav.veluri00@gmail.com>
2019-08-20 16:03:54 -0700

    Testcase for running specint

---
+++ cl_manycore/regression/specint/spmd_tests.h
@@ -0,0 +1,11 @@
+#ifndef __SPMD_TESTS_H
+#define __SPMD_TESTS_H
+
+#include <bsg_manycore.h>
+#include <bsg_manycore_tile.h>
+#include <bsg_manycore_loader.h>
+#include <bsg_manycore_errno.h>	
+
+#include "../cl_manycore_regression.h"
+
+#endif // __SPMD_TESTS_H
