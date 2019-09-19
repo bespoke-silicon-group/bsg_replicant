601022792534d6da8dd5002da3e62064a67a05b0
Dustin Richmond <dustinar@uw.edu>
2019-09-19 09:37:25 -0700

    Fixed xilinx-specific synthesis issues

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -61,6 +61,10 @@ include $(BSG_MANYCORE_DIR)/machines/arch_filelist.mk
 # but transformed into a tool-specific syntax where necesssary.
 VINCLUDES += $(HARDWARE_PATH)
 
+VSOURCES += $(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_cache_wrapper_axi.v
+VSOURCES += $(BSG_MANYCORE_DIR)/testbenches/common/v/memory_system.v
+VSOURCES += $(BSG_MANYCORE_DIR)/testbenches/common/v/bsg_mem_cfg_pkg.v
+
 VSOURCES += $(HARDWARE_PATH)/bsg_bladerunner_configuration.v
 VSOURCES += $(HARDWARE_PATH)/cl_manycore_pkg.v
 VSOURCES += $(HARDWARE_PATH)/$(CL_TOP_MODULE).sv
