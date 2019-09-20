8faaa08b92d2537b7f56c7352368e57f830e93fa
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-09-20 23:06:49 +0000

    Added floating point vector square root cuda regression test.

--- regression/cuda/tests.mk
+++ regression/cuda/tests.mk
@@ -72,6 +72,7 @@ INDEPENDENT_TESTS += test_float_vec_div
 INDEPENDENT_TESTS += test_float_matrix_mul
 INDEPENDENT_TESTS += test_float_matrix_mul_shared_mem
 INDEPENDENT_TESTS += test_float_vec_exp
+INDEPENDENT_TESTS += test_float_vec_sqrt
 
 # REGRESSION_TESTS is a list of all regression tests to run.
 REGRESSION_TESTS = $(UNIFIED_TESTS) $(INDEPENDENT_TESTS)
