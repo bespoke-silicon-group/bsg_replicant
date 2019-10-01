6fa3ff5d1d5b8a849f9d13c9b410d05d7ca9ccbd
Sasha Krassovsky <samovar@uw.edu>
2019-09-30 17:06:09 -0700

    Add test_softmax

---
+++ regression/cuda/test_softmax.h
@@ -0,0 +1,14 @@
+#ifndef TEST_SOFTMAX_H
+#define TEST_SOFTMAX_H
+
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+#include <stdlib.h>
+
+#include "cuda_tests.h"
+
+
+#endif
