2a72ee4606e95f7a15d568844065100965664bab
Dustin Richmond <dustinar@uw.edu>
2019-10-30 10:00:58 -0700

    fixup! Purging tabs

--- regression/cl_manycore_regression.h
+++ regression/cl_manycore_regression.h
@@ -1,4 +1,4 @@
-x// Copyright (c) 2019, University of Washington All rights reserved.
+// Copyright (c) 2019, University of Washington All rights reserved.
 // 
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
