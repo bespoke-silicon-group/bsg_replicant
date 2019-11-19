600abb26731d5fb0b99427037b11b554cea2e255
Sasha Krassovsky <samovar@uw.edu>
2019-11-19 15:25:32 -0800

    Add test_conv1d (#462)

--- regression/cuda/test_conv1d.c
+++ regression/cuda/test_conv1d.c
@@ -25,33 +25,65 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include "test_softmax.h"
+#include "test_conv1d.h"
 #include <math.h>
 
 #define ALLOC_NAME "default_allocator"
 
-#define max(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN_N 4
+#define MAX_N 1024
 
-void softmax(float *A, float *B, int M, int N)
+#define MIN_F 1
+#define MAX_F 128
+
+#define MIN_P 0
+#define MAX_P 8
+
+#define MIN_S 1
+#define MAX_S 8
+
+#define MAX_VALUE_MAGNITUDE 1e6
+
+int generate_inclusive(int min, int max)
 {
-        float m = -INFINITY;
-        for(int i = 0; i < M * N; i++)
-                m = max(A[i], m);
-
-        for(int i = 0; i < M * N; i++)
-                B[i] = expf(A[i] - m);
-
-        float sum = 0;
-        for(int i = 0; i < M * N; i++)
-                sum += B[i];
-
-        for(int i = 0; i < M * N; i++)
-                B[i] /= sum;
+        int range = (max - min + 1);
+        return (rand() % range) + min;
 }
 
-int kernel_softmax(int argc, char **argv)
+int compute_M(int N, int F, int P, int S)
+{
+        return 1 + (N - F + 2 * P) / S;
+}
+
+void conv1d(const float *A,
+            const int N,
+            const float *filter,
+            const int F,
+            const int P,
+            float *B,
+            const int S)
+{
+        int M = compute_M(N, F, P, S);
+        for(int i = 0; i < M; i++)
+        {
+                int window_idx = i * S;
+                float res = 0;
+                for(int j = 0; j < F; j++)
+                {
+                        int a_idx = window_idx - P + j;
+                        float a = 0;
+                        if(0 <= a_idx && a_idx < N)
+                                a = A[a_idx];
+
+                        res += filter[j] * a;
+                }
+                B[i] = res;
+        }
+}
+
+int kernel_conv1d(int argc, char **argv)
 {       
-        bsg_pr_test_info("Running CUDA Softmax Kernel on a 2x2 tile group.\n\n");
+        bsg_pr_test_info("Running CUDA Conv1D Kernel on a 2x2 tile group.\n\n");
         char *elf, *test_name;
         struct arguments_path args = { NULL, NULL };
         argp_parse(&argp_path, argc, argv, 0, 0, &args);
@@ -75,47 +107,76 @@ int kernel_softmax(int argc, char **argv)
                 return rc;
         }
 
-        int M = 32;
-        int N = 64;
-        eva_t A_device, B_device;
-        rc = hb_mc_device_malloc(mc, M * N * sizeof(float), &A_device);
+        int N = generate_inclusive(MIN_N, MAX_N);
+        int F = generate_inclusive(MIN_F, MAX_F < N ? MAX_F : N);
+        int P = generate_inclusive(MIN_P, MAX_P);
+        int S = generate_inclusive(MIN_S, MAX_S);
+        int M = compute_M(N, F, P, S);
+        
+        size_t A_size      = sizeof(float) * N;
+        size_t filter_size = sizeof(float) * F;
+        size_t B_size      = sizeof(float) * M;
+        
+        eva_t A_device, B_device, filter_device;
+        rc = hb_mc_device_malloc(mc, A_size, &A_device);
         if(rc != HB_MC_SUCCESS)
         {
                 bsg_pr_err("Failed to allocate A on the manycore.\n");
                 return rc;
         }
 
-        rc = hb_mc_device_malloc(mc, M * N * sizeof(float), &B_device);
+        rc = hb_mc_device_malloc(mc, filter_size, &filter_device);
+        if(rc != HB_MC_SUCCESS)
+        {
+                bsg_pr_err("Failed to allocate B on the manycore.\n");
+                return rc;
+        }
+        
+        rc = hb_mc_device_malloc(mc, B_size, &B_device);
         if(rc != HB_MC_SUCCESS)
         {
                 bsg_pr_err("Failed to allocate B on the manycore.\n");
                 return rc;
         }
 
-        float A_host[M * N];
-        for(int i = 0; i < M * N; i++)
+        float A_host[N];
+        for(int i = 0; i < sizeof(A_host) / sizeof(A_host[0]); i++)
         {
-                A_host[i] = hb_mc_generate_float_rand();
+                A_host[i] = fmod(hb_mc_generate_float_rand(), MAX_VALUE_MAGNITUDE);
                 bsg_pr_test_info("A_host[%d] = %.9f \n",
                                  i, A_host[i]);
         }
 
-        rc = hb_mc_device_memcpy(mc, A_device, A_host, M * N * sizeof(float), HB_MC_MEMCPY_TO_DEVICE);
+        float filter_host[F];
+        for(int i = 0; i < sizeof(filter_host) / sizeof(filter_host[0]); i++)
+        {
+                filter_host[i] = fmod(hb_mc_generate_float_rand(), MAX_VALUE_MAGNITUDE);
+                bsg_pr_test_info("filter_host[%d] = %.9f \n",
+                                 i, filter_host[i]);
+        }
+        
+        rc = hb_mc_device_memcpy(mc, A_device, A_host, A_size, HB_MC_MEMCPY_TO_DEVICE);
         if(rc != HB_MC_SUCCESS)
         {
                 bsg_pr_err("Failed to memcpy A to the manycore.\n");
                 return rc;
         }
+        
+        rc = hb_mc_device_memcpy(mc, filter_device, filter_host, filter_size, HB_MC_MEMCPY_TO_DEVICE);
+        if(rc != HB_MC_SUCCESS)
+        {
+                bsg_pr_err("Failed to memcpy filter to the manycore.\n");
+                return rc;
+        }
 
-        uint32_t block_size_x = N;
-        uint32_t block_size_y = M;
+        uint32_t block_size = M;
 
         hb_mc_dimension_t tilegroup_dim = { .x = 2, .y = 2 };
         hb_mc_dimension_t grid_dim = { .x = 1, .y = 1 };
 
-        int cuda_argv[] = { A_device, B_device, M, N, block_size_y, block_size_x };
+        int cuda_argv[] = { A_device, N, filter_device, F, P, B_device, S, block_size };
         size_t cuda_argc = sizeof(cuda_argv) / sizeof(cuda_argv[0]);
-        rc = hb_mc_kernel_enqueue(mc, grid_dim, tilegroup_dim, "kernel_softmax", cuda_argc, cuda_argv);
+        rc = hb_mc_kernel_enqueue(mc, grid_dim, tilegroup_dim, "kernel_conv1d", cuda_argc, cuda_argv);
         if(rc != HB_MC_SUCCESS)
         {
                 bsg_pr_err("Failed to initialize grid.\n");
@@ -129,8 +190,8 @@ int kernel_softmax(int argc, char **argv)
                 return rc;
         }
 
-        float B_actual[M * N];
-        rc = hb_mc_device_memcpy(mc, B_actual, B_device, M * N * sizeof(float), HB_MC_MEMCPY_TO_HOST);
+        float B_actual[M];
+        rc = hb_mc_device_memcpy(mc, B_actual, B_device, B_size, HB_MC_MEMCPY_TO_HOST);
         if(rc != HB_MC_SUCCESS)
         {
                 bsg_pr_err("Failed to mempcy result to host.\n");
@@ -144,31 +205,30 @@ int kernel_softmax(int argc, char **argv)
                 return rc;
         }
 
-        float B_expected[M * N];
-        softmax(A_host, B_expected, M, N);
+        float B_expected[M];
+        conv1d(A_host, N, filter_host, F, P, B_expected, S);
 
         int mismatches = 0;
-        for(int y = 0; y < M; y++)
-                for(int x = 0; x < N; x++)
+
+        bsg_pr_info("(N, F, P, S, M) = (%d, %d, %d, %d, %d)\n", N, F, P, S, M);
+        for(int i = 0; i < M; i++)
+                if(!hb_mc_floats_match(B_actual[i], B_expected[i]))
                 {
-                        if(hb_mc_calculate_float_error(B_actual[y * N + x], B_expected[y * N + x]) > MAX_FLOAT_ERROR_TOLERANCE ||
-                           (!isnormal(B_actual[y * N + x]) && B_actual[y * N + x] != 0.0f))
-                        {
-                                bsg_pr_err(BSG_RED("Mismatch: ") "B[%d][%d] = %.9f = %x \t Expected: %.9f = %x\n",
-                                           y, x,
-                                           B_actual[y * N + x],
-                                           B_actual[y * N + x],
-                                           B_expected[y * N + x],
-                                           B_expected[y * N + x]);
-                                mismatches++;
-                        }
+                        bsg_pr_err(BSG_RED("Mismatch: ") "B[%d] = %.9f = 0x%x \t Expected: %.9f = 0x%x\n",
+                                   i,
+                                   B_actual[i],
+                                   *((uint32_t *)(B_actual + i)),
+                                   B_expected[i],
+                                   *((uint32_t *)(B_expected + i)));
+                        mismatches++;
                 }
+
         if(!mismatches)
         {
-                bsg_pr_test_info(BSG_GREEN("Matrices match!\n"));
+                bsg_pr_test_info(BSG_GREEN("Vectors match!\n"));
                 return HB_MC_SUCCESS;
         }
-        bsg_pr_test_err(BSG_RED("Matrices don't match!\n"));
+        bsg_pr_test_err(BSG_RED("Vectors don't match!\n"));
         return HB_MC_FAIL;
 }
 
@@ -188,8 +248,8 @@ void cosim_main(uint32_t *exit_code, char *args)
         scope = svGetScopeFromName("tb");
         svSetScope(scope);
 #endif
-        bsg_pr_test_info("test_softmax Regression Test (COSIMULATION)\n");
-        int rc = kernel_softmax(argc, argv);
+        bsg_pr_test_info("test_conv1d Regression Test (COSIMULATION)\n");
+        int rc = kernel_conv1d(argc, argv);
         *exit_code = rc;
         bsg_pr_test_pass_fail(rc == HB_MC_SUCCESS);
         return;
@@ -197,8 +257,8 @@ void cosim_main(uint32_t *exit_code, char *args)
 #else
 int main(int argc, char **argv)
 {
-        bsg_pr_test_info("test_softmax Regression Test (F1)\n");
-        int rc = kernel_softmax(argc, argv);
+        bsg_pr_test_info("test_conv1d Regression Test (F1)\n");
+        int rc = kernel_conv1d(argc, argv);
         bsg_pr_test_pass_fail(rc == HB_MC_SUCCESS);
         return rc;
 }
