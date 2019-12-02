1e777823ed3972bd8bade9d25acdfd4b81ecacf6
Seyed Borna Ehsani <behsani@cs.washington.edu>
2019-12-02 14:34:59 -0800

    Modified cosimulation Makefile according to stats files name changes.

--- testbenches/cosimulation.mk
+++ testbenches/cosimulation.mk
@@ -54,9 +54,9 @@ VANILLA_LOG_TARGETS =$(addprefix $(EXEC_PATH)/,$(VANILLA_LOG_RULES))
 $(VANILLA_LOG_RULES): SIM_ARGS +=+trace
 $(VANILLA_LOG_RULES): %.vanilla.log: $(EXEC_PATH)/%.log
 	@mv vanilla.log $@
-	@mv vcache_stats.log $(subst vanilla,vcache_stats,$@)
-	@mv vanilla_stall_trace.log $(subst vanilla,vanilla_stall_trace,$@)
-	@mv vanilla_stats.log $(subst vanilla,vanilla_stats,$@)
+	@mv vcache_non_blocking_stats.log $(subst vanilla,vcache_non_blocking_stats,$@)
+	@mv vanilla_operation_trace.log $(subst vanilla,vanilla_operation_trace,$@)
+	@mv vanilla_stats.csv $(subst vanilla,vanilla_stats.csv,$@)
 
 clean: regression.clean compilation.clean $(USER_CLEAN_RULES)
-	rm -rf *.log
+	rm -rf *.log *.csv
