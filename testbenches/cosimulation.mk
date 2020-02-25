ef4fc3a208bbfd587efdf276fe6bfa43748047e1
Dustin Richmond <dustinar@uw.edu>
2020-02-25 15:25:43 -0800

    Propogating changes to all other test suites

--- testbenches/cosimulation.mk
+++ testbenches/cosimulation.mk
@@ -73,6 +73,10 @@ help:
 	@echo "make {regression|clean|<test_name>|<test_name>.log}"
 	@echo "      regression: Run all cosimulation regression tests for "
 	@echo "             this subdirectory"
+	@echo "      waveforms: Run all cosimulation regression tests for "
+	@echo "             this subdirectory and generate waveforms"
+	@echo "      traces: Run all cosimulation regression tests for "
+	@echo "             this subdirectory and generate traces"
 	@echo "      <test_name>: Build the cosimulation binary for a specific"
 	@echo "             test"
 	@echo "      <test_name.log>: Run a specific cosimulation test and "
@@ -84,17 +88,46 @@ help:
 	@echo ""
 	@echo "      clean: Remove all subdirectory-specific outputs"
 
-SIM_ARGS += +rad +ntb_random_seed_automatic 
+waveforms: $(REGRESSION_TESTS:%=$(EXEC_PATH)/%.vpd)
+traces: $(REGRESSION_TESTS:%=$(EXEC_PATH)/%.vanilla.log)
+test_loader $(REGRESSION_TESTS): %: $(EXEC_PATH)/%
 
+$(EXEC_PATH)/%.log: %.vanilla_stats.csv ;
+
+SIM_ARGS += +rad +ntb_random_seed_automatic 
+$(EXEC_PATH)/%.vpd: SIM_ARGS += +vcs+vcdpluson +vcs+vcdplusmemon +memcbk
+$(EXEC_PATH)/%.vpd: SIM_ARGS += +vpdfile+$@
+
+$(UNIFIED_TESTS:%=%.vpd): $(EXEC_PATH)/test_loader.debug
+$(INDEPENDENT_TESTS:%=%.vpd): %.vpd : $(EXEC_PATH)/%.debug
+$(REGRESSION_TESTS:%=%.vpd): %.vpd : %.rule
+
+$(UNIFIED_TESTS:%=%.vanilla_stats.csv): $(EXEC_PATH)/test_loader
+$(INDEPENDENT_TESTS:%=%.vanilla_stats.csv): %.vanilla_stats.csv : $(EXEC_PATH)/%
+$(REGRESSION_TESTS:%=%.vanilla_stats.csv): %.vanilla_stats.csv : %.rule
+
+%.vanilla_stats.csv %.vpd:
+	mkdir -p .$(TEST_NAME)
+	cd .$(TEST_NAME) && \
+	$< 2>&1 $(SIM_ARGS) +c_args="$(C_ARGS)" | tee $(EXEC_PATH)/$(TEST_NAME).log
+	@mv .$(TEST_NAME)/vanilla_stats.csv $(TEST_NAME).vanilla_stats.csv
+
+%.vanilla_operation_trace.csv %.vanilla.log: TEST_NAME = $(basename $(basename $(notdir $@)))
 %.vanilla_operation_trace.csv %.vanilla.log: SIM_ARGS += +trace
 %.vanilla_operation_trace.csv %.vanilla.log: $(EXEC_PATH)/%.log
-	$(eval TEST_NAME = $(basename $(basename $(notdir $@))))
 	@mv $(dir $@)/.$(TEST_NAME)/vanilla.log $(TEST_NAME).vanilla.log
 	@mv $(dir $@)/.$(TEST_NAME)/vanilla_operation_trace.csv $(TEST_NAME).vanilla_operation_trace.csv
 
-test_loader $(REGRESSION_TESTS): %: $(EXEC_PATH)/%
+
+%.dve: %.vpd
+	$(DVE) -full64 -vpd $< &
 
 clean: regression.clean compilation.clean link.clean $(USER_CLEAN_RULES)
 	rm -rf *.log
 	rm -rf *.vanilla_operation_trace.csv *.vanilla_stats.csv
-	rm -rf $(REGRESSION_TESTS) test_loader
+	rm -rf $(REGRESSION_TESTS) test_loader 
+	rm -rf $(REGRESSION_TESTS:%=%.debug) test_loader.debug
+	rm -rf $(REGRESSION_TESTS:%=.%)
+
+
+.PHONY: %.dve
