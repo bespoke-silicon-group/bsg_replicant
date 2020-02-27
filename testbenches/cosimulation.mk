cf0044f9f6cd70905cbb20917d17a1b1623c3200
Dustin Richmond <dustinar@uw.edu>
2020-02-27 15:35:32 -0800

    Added features, documentation
    * Changed dependency structure so that both waveforms and traces can
      be generated simultaneously.

--- testbenches/cosimulation.mk
+++ testbenches/cosimulation.mk
@@ -70,53 +70,148 @@ include $(REGRESSION_PATH)/regression.mk
 .DEFAULT_GOAL := help
 help:
 	@echo "Usage:"
-	@echo "make {regression|clean|<test_name>|<test_name>.log}"
+	@echo "make {target(s)}"
 	@echo "      regression: Run all cosimulation regression tests for "
-	@echo "             this subdirectory"
-	@echo "      waveforms: Run all cosimulation regression tests for "
-	@echo "             this subdirectory and generate waveforms"
-	@echo "      traces: Run all cosimulation regression tests for "
-	@echo "             this subdirectory and generate traces"
+	@echo "             this subdirectory and print pass/fail results"
+	@echo ""
+	@echo "      regression+traces: Run all cosimulation regression tests"
+	@echo "             for this subdirectory and generate traces for each"
+	@echo "             test. Does not print aggregate pass/fail results."
+	@echo ""
+	@echo "      regression+waveforms: Run all cosimulation regression "
+	@echo "             tests for this subdirectory and generate waveforms"
+	@echo "             for each test. Does not print aggregate pass/fail"
+	@echo "             results."
+	@echo ""
+	@echo "      regression+traces+waveforms: Run all cosimulation "
+	@echo "             regression tests for this subdirectory and generate"
+	@echo "             traces and waveforms for each test. Does not print"
+	@echo "             aggregate pass/fail results."
+	@echo ""
 	@echo "      <test_name>: Build the cosimulation binary for a specific"
 	@echo "             test"
-	@echo "      <test_name.log>: Run a specific cosimulation test and "
+	@echo ""
+	@echo "      <test_name>.log: Run a specific cosimulation test and "
 	@echo "             generate the log file (will not generate traces)"
-	@echo "      <test_name.vanilla.log>: Run a specific cosimulation test and "
-	@echo "             generate the vanilla.log and vanilla_operation_trace.csv "
-	@echo "             and vanilla_operation_trace.csv file"
-	@echo "      <test_name.vanilla_operation_trace.csv>: Same as above"
+	@echo ""
+	@echo "      <test_name>.vanilla.log: Run a specific cosimulation test"
+	@echo "             and generate the vanilla.log file."
+	@echo ""
+	@echo "      <test_name>.vanilla_operation_trace.csv: Run a specific"
+	@echo "             cosimulation test and generate the"
+	@echo "             vanilla_operation_trace.csv file."
 	@echo ""
 	@echo "      clean: Remove all subdirectory-specific outputs"
+	@echo ""
+	@echo "      help: Print this message (default)"
+	@echo ""
+	@echo "Available <test_name> targets:"
+	@echo "$(REGRESSION_TESTS)" | fold -s -w70 | sed -e "s|^ ||g;s|^|\t|g"
 
-waveforms: $(REGRESSION_TESTS:%=%.vpd)
-traces: $(REGRESSION_TESTS:%=%.vanilla.log)
-test_loader $(REGRESSION_TESTS): %: $(EXEC_PATH)/%
 
+# Define the global targets, as documented above
+regression+traces: $(REGRESSION_TESTS:%=%.vanilla.log) 
+regression+traces: $(REGRESSION_TESTS:%=%.vanilla_operation_trace.csv)
+regression+traces: $(REGRESSION_TESTS:%=%.vanilla_stats.csv)
+
+regression+waveforms: $(REGRESSION_TESTS:%=%.debug.log)
+
+# By adding +trace to SIM_ARGS this SHOULD will produce the trace files. By
+# running regression+waveforms first it will produce traces AND waveforms and
+# prevent regression+traces from re-running. #makemagic
+regression+traces+waveforms: SIM_ARGS += +trace
+regression+traces+waveforms: regression+waveforms regression+traces
+
+# This defines "local" goals for each regression test target
+test_loader $(INDEPENDENT_TESTS): %: $(EXEC_PATH)/%
+
+# All simulations should run with +ntb_random_seed_automatic.
+# 
+# From the VCS MX User-Guide: +ntb_random_seed_automatic Picks a unique value to
+# supply as the first seed used by a testbench. The value is determined by
+# combining the time of day, host name and process id. This ensures that no two
+# simulations have the same starting seed.
+SIM_ARGS += +ntb_random_seed_automatic 
+
+#$(EXEC_PATH)/%.debug.log: %.vpd ;
+
+# OK, here's where things get complicated. 
+
+# Each regression that generates a waveform generates it in the .<test_name>
+# directory, named vcdplus.vpd. To generate a waveform we must provide the
+# following arguments to the simulation through SIM_ARGS.
+.%/vcdplus.vpd: SIM_ARGS += +vcs+vcdpluson +vcs+vcdplusmemon +memcbk
+
+
+# Generating a waveform requires a simulation binary compiled with different VCS
+# flags. We call this binary <test_name>.debug, and its build rule is defined in
+# link.mk.
+#
+# UNIFIED_TESTS all use test_loader.debug. INDEPENDENT_TESTS use their own
+# top-level. Encode dependencies for each differently.
+#
+# The ORDER of these dependencies are important because the actual rule uses $<
+# to get the name of the executable.
+$(UNIFIED_TESTS:%=.%/vcdplus.vpd): $(EXEC_PATH)/test_loader.debug
+$(INDEPENDENT_TESTS:%=.%/vcdplus.vpd):.%/vcdplus.vpd : $(EXEC_PATH)/%.debug
+# Each regression test can define its dependencies in <test_name>.rule, but we
+# must satisfy that rule before generating the .vpd file
+$(REGRESSION_TESTS:%=.%/vcdplus.vpd): .%/vcdplus.vpd : %.rule
+
+# Normal cosimulation requires a simulation binary without waveform flags (it
+# executes faster...). We call this binary <test_name>, and its build rule is
+# defined in link.mk.
+#
+# UNIFIED_TESTS all use test_loader. INDEPENDENT_TESTS use their own top-level
+# that corresponds to their <test_name>. Encode dependencies for each
+# differently.
+#
+# The ORDER of these dependencies are important because the actual rule uses $<
+# to get the name of the executable.
+$(UNIFIED_TESTS:%=.%/vanilla_stats.csv): $(EXEC_PATH)/test_loader
+$(INDEPENDENT_TESTS:%=.%/vanilla_stats.csv): .%/vanilla_stats.csv : $(EXEC_PATH)/%
+# Each regression test can define its dependencies in <test_name>.rule, but we
+# must satisfy that rule before generating the .csv file
+$(REGRESSION_TESTS:%=.%/vanilla_stats.csv): .%/vanilla_stats.csv : %.rule
+
+# These are the execution rules for the binaries. We define TEST_NAME so that it
+# can be used in C_ARGS, and LOG_NAME so that we can write a log. If a waveform
+# is being generated, then it writes to <test_name>.debug.log, otherwise
+# <test_name>.log. Finally, depend on <test_name>.rule so that we rebuild the
+# RISC-V binaries.
+.%/vcdplus.vpd .%/vanilla_stats.csv: TEST_NAME=$(@:.%/$(notdir $@)=%)
+.%/vcdplus.vpd .%/vanilla_stats.csv: LOG_NAME=$(subst ..,.,$(TEST_NAME).$(suffix $<).log)
+.%/vcdplus.vpd .%/vanilla_stats.csv:
+	mkdir -p $(dir $@)
+	cd $(dir $@) && \
+	$< $(SIM_ARGS) +c_args="$(C_ARGS)" 2>&1 | tee $(LOG_NAME)
+	@cp $(dir $@)/$(LOG_NAME) $(LOG_NAME)
+
+# %.debug.log is just an alias for %.vpd
+%.debug.log: %.vpd ;
+# Running simulation generates <test_name>/vcdplus.vpd. Move it to
+# <test_name>.vpd
+%.vpd: .%/vcdplus.vpd
+	@mv $< $@
+
+# $(EXEC_PATH)/%.log is an alias for %.vanilla_stats.csv. 
 $(EXEC_PATH)/%.log: %.vanilla_stats.csv ;
+# vanilla_stats.csv is generated in .<test_name>, so we move it out and into a
+# name-specific file.
+%.vanilla_stats.csv: .%/vanilla_stats.csv
+	@mv $< $@
 
-SIM_ARGS += +rad +ntb_random_seed_automatic 
-%.vpd: SIM_ARGS += +vcs+vcdpluson +vcs+vcdplusmemon +memcbk
-%.vpd: SIM_ARGS += +vpdfile+$@
+# .%/vanilla_operation_trace.csv and .%/vanilla.log are generated by running a
+# simulation with the +trace flag. They are generated into the .<test_name>
+# directory, so we copy them out into %.vanilla.log and
+# %.vanilla_operation_trace.csv to make them test-specific.
+.%/vanilla_operation_trace.csv .%/vanilla.log: SIM_ARGS += +trace
+.%/vanilla_operation_trace.csv .%/vanilla.log: %.vanilla_stats.csv ;
+%.vanilla_operation_trace.csv: .%/vanilla_operation_trace.csv
+	@mv $< $@
 
-$(UNIFIED_TESTS:%=%.vpd): $(EXEC_PATH)/test_loader.debug
-$(INDEPENDENT_TESTS:%=%.vpd): %.vpd : $(EXEC_PATH)/%.debug
-$(REGRESSION_TESTS:%=%.vpd): %.vpd : %.rule
-
-$(UNIFIED_TESTS:%=%.vanilla_stats.csv): $(EXEC_PATH)/test_loader
-$(INDEPENDENT_TESTS:%=%.vanilla_stats.csv): %.vanilla_stats.csv : $(EXEC_PATH)/%
-$(REGRESSION_TESTS:%=%.vanilla_stats.csv): %.vanilla_stats.csv : %.rule
-
-%.vanilla_stats.csv %.vpd:
-	mkdir -p .$(TEST_NAME)
-	cd .$(TEST_NAME) && \
-	$< 2>&1 $(SIM_ARGS) +c_args="$(C_ARGS)" | tee $(EXEC_PATH)/$(TEST_NAME).log
-	@mv .$(TEST_NAME)/vanilla_stats.csv $(TEST_NAME).vanilla_stats.csv
-
-%.vanilla_operation_trace.csv %.vanilla.log: TEST_NAME = $(basename $(basename $(notdir $@)))
-%.vanilla_operation_trace.csv %.vanilla.log: SIM_ARGS += +trace
-%.vanilla_operation_trace.csv %.vanilla.log: $(EXEC_PATH)/%.log
-	@mv .$(TEST_NAME)/vanilla.log $(TEST_NAME).vanilla.log
-	@mv .$(TEST_NAME)/vanilla_operation_trace.csv $(TEST_NAME).vanilla_operation_trace.csv
+%.vanilla.log: .%/vanilla.log
+	@mv $< $@
 
 
 %.dve: %.vpd
@@ -131,3 +226,4 @@ clean: regression.clean compilation.clean link.clean $(USER_CLEAN_RULES)
 
 
 .PHONY: %.dve
+.PRECIOUS: %.csv %.vanilla_stats.csv %.log %.debug.log %.vpd
