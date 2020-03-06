937e9f2547a0dfeaf41fd9d1b9b869f236106a30
Max Ruttenberg <mrutt@cs.washington.edu>
2020-03-06 10:37:19 -0800

    removes tabs from test_bsg_scalar_print.c

--- regression/spmd/test_bsg_scalar_print.c
+++ regression/spmd/test_bsg_scalar_print.c
@@ -106,7 +106,7 @@ int test_scalar_print (int argc, char **argv) {
 
         /* initialize the tile */
         hb_mc_coordinate_t target
-	    = hb_mc_config_get_origin_vcore(hb_mc_manycore_get_config(mc));
+            = hb_mc_config_get_origin_vcore(hb_mc_manycore_get_config(mc));
         hb_mc_coordinate_t origin = target;
 
         // freeze the tile
