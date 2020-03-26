6e186532075b2aaf20c2677f853273e95dbbaa58
Max Ruttenberg <mrutt@cs.washington.edu>
2020-03-25 18:04:20 -0700

    adds missing header

---
+++ libraries/bsg_manycore_rom.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <bsg_manycore_features.h>
+#ifndef __cplusplus
+#include <stdint.h>
+#else
+#include <cstdint>
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    typedef uint32_t hb_mc_rom_word_t;
+
+#ifdef __cplusplus
+}
+#endif
