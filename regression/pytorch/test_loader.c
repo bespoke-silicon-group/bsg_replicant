de319723511a80867fcfcc6bf90349e4edb31dbe
Bandhav Veluri <bandhav.veluri00@gmail.com>
2020-04-21 09:51:48 -0700

    Python cmd args (#558)
    * Support for pytorch cmd line arguments
    
    * renamed test file
    
    * pytorch add script path to sys path
    
    * Call py_main instead of running a file

--- regression/pytorch/test_loader.c
+++ regression/pytorch/test_loader.c
@@ -1,19 +1,19 @@
 // Copyright (c) 2019, University of Washington All rights reserved.
-// 
+//
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
-// 
+//
 // Redistributions of source code must retain the above copyright notice, this list
 // of conditions and the following disclaimer.
-// 
+//
 // Redistributions in binary form must reproduce the above copyright notice, this
 // list of conditions and the following disclaimer in the documentation and/or
 // other materials provided with the distribution.
-// 
+//
 // Neither the name of the copyright holder nor the names of its contributors may
 // be used to endorse or promote products derived from this software without
 // specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 // ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 // WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -31,49 +31,29 @@
 #include <string.h>
 #include <time.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "pytorch_tests.h"
 
 int test_pytorch(int argc, char **argv) {
-        int len, err;
-        char *pytorch_path;
-        char *test_name;
-
-        struct arguments_path args = {NULL};
-
-        argp_parse (&argp_path_py, argc, argv, 0, 0, &args);
-        test_name = args.name;
-        pytorch_path = args.path;
-        bsg_pr_test_info("%s Regression Test\n", test_name);
-
-        len = strlen(pytorch_path) + strlen(test_name) + strlen(".py");
-
-        char program_path[len + 1], *ptr;
-        program_path[len] = '\0';
-        ptr = program_path;
-
-        strcpy(ptr, pytorch_path);
-        ptr += strlen(pytorch_path);
-
-        strcpy(ptr, test_name);
-        ptr += strlen(test_name);
-
-        strcpy(ptr, ".py");
-        ptr += strlen(".py");
-        *ptr = '\0';
-
+        // Initialize the interpreter
         Py_Initialize();
 
-        PyObject *obj = Py_BuildValue("s", program_path);
-        FILE *fp = _Py_fopen_obj(obj, "r+");
+        // Append cwd to sys path
+        char* cwd[PATH_MAX];
+        PyObject* sysPath = PySys_GetObject((char*)"path");
+        PyObject* programName = PyUnicode_FromString(getcwd(cwd, sizeof(cwd)));
+        PyList_Append(sysPath, programName);
+        Py_DECREF(programName);
 
-        err = PyRun_SimpleFileEx(fp, program_path, 0);
+        // Set Python sys argv
+        int py_argc = argc;
+        wchar_t* py_argv[py_argc];
 
-        if (Py_FinalizeEx() < 0) {
-                exit(120);
+        for(int i = 0; i < py_argc; ++i) {
+                py_argv[i] = Py_DecodeLocale(argv[i], NULL);
         }
-        fclose(fp);
 
-        return err;
+        return Py_Main(py_argc, py_argv);
 }
 
 #ifdef COSIM
