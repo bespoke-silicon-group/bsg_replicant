cdd2f055133db11413a9d8109461d936f9451ec2
Dustin Richmond <dustinar@uw.edu>
2020-04-28 17:03:38 -0700

    fixup! fixup! Simplified main/cosim main calling

--- regression/library/test_printing.c
+++ regression/library/test_printing.c
@@ -47,7 +47,6 @@
 #include "test_printing.h"
 static int test_printing(void)
 {
-        int rc;
         hb_mc_manycore_t mc = {0};
         rc = hb_mc_manycore_init(&mc, "manycore@test_rom", 0);
         if(rc != HB_MC_SUCCESS){
