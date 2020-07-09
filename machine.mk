a52d5bfa1f804b6b7d0baf445e4a986e021469ab
Dustin Richmond <dustinar@uw.edu>
2020-07-09 14:01:25 -0700

    Switched default machine

--- machine.mk
+++ machine.mk
@@ -38,7 +38,7 @@ endif
 
 # To switch machines, simply switch the path of BSG_MACHINE_PATH to
 # another directory with a Makefile.machine.include file.
-BSG_MACHINE_PATH ?= $(BSG_F1_DIR)/machines/baseline_v0_8_4
+BSG_MACHINE_PATH ?= $(BSG_F1_DIR)/machines/baseline_v0_16_8
 
 # Convert the machine path to an abspath
 override BSG_MACHINE_PATH := $(abspath $(BSG_MACHINE_PATH))
