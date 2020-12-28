495bfe8e3555d68ec3ee2e5ebfa0f784ddcea6a0
Dustin Richmond <dustinar@uw.edu>
2020-12-28 15:03:28 -0800

    Fix sizing issue with test_rom.c

--- examples/library/test_rom.c
+++ examples/library/test_rom.c
@@ -35,7 +35,8 @@ int test_rom (int argc, char **argv) {
         uint32_t vcache_assoc_expect_max = 32;
         uint32_t vcache_sets_expect = BSG_MANYCORE_MACHINE_VCACHE_BANK_SETS;
         uint32_t major_version_max = 4; uint32_t minor_version_max = 10, minor_version_min = 0;
-        uint32_t dram_size = 0, dram_bank_size = 0, n_dram_co = 0;
+        size_t dram_size = 0, dram_bank_size = 0;
+        uint32_t n_dram_co = 0;
         hb_mc_dimension_t dim;
         hb_mc_coordinate_t host;
         const hb_mc_config_t *config;
