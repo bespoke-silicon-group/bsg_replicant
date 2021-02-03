cb5fd8b26a57f316eeb6a2ebe261e6ad5a8a8338
Max Ruttenberg <mrutt@cs.washington.edu>
2021-02-03 01:10:42 -0800

    Re-inserts NPA checks

--- libraries/bsg_manycore.cpp
+++ libraries/bsg_manycore.cpp
@@ -381,21 +381,21 @@ static bool hb_mc_manycore_dst_npa_is_valid(hb_mc_manycore_t *mc, const hb_mc_np
         const hb_mc_config_t *cfg = hb_mc_manycore_get_config(mc);
         hb_mc_dimension_t dim = hb_mc_config_get_dimension_network(cfg);
 
-        // if (hb_mc_npa_get_x(npa) >= hb_mc_dimension_get_x(dim)) {
-        //         char npa_str[256];
-        //         manycore_pr_err(mc, "%s: %s is not a valid destination\n",
-        //                         __func__,
-        //                         hb_mc_npa_to_string(npa, npa_str, sizeof(npa_str)));
-        //         return false;
-        // }
+        if (hb_mc_npa_get_x(npa) >= hb_mc_dimension_get_x(dim)) {
+                char npa_str[256];
+                manycore_pr_err(mc, "%s: %s is not a valid destination\n",
+                                __func__,
+                                hb_mc_npa_to_string(npa, npa_str, sizeof(npa_str)));
+                return false;
+        }
 
-        // if (hb_mc_npa_get_y(npa) >= hb_mc_dimension_get_y(dim)) {
-        //         char npa_str[256];
-        //         manycore_pr_err(mc, "%s: %s is not a valid destination\n",
-        //                         __func__,
-        //                         hb_mc_npa_to_string(npa, npa_str, sizeof(npa_str)));
-        //         return false;
-        // }
+        if (hb_mc_npa_get_y(npa) >= hb_mc_dimension_get_y(dim)) {
+                char npa_str[256];
+                manycore_pr_err(mc, "%s: %s is not a valid destination\n",
+                                __func__,
+                                hb_mc_npa_to_string(npa, npa_str, sizeof(npa_str)));
+                return false;
+        }
 
         return true;
 }
