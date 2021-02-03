6cbfd4007976d5386c523d76198389f082afed0c
Max Ruttenberg <mrutt@cs.washington.edu>
2021-02-03 14:11:29 -0800

    Removes commented out makefile lines

--- libraries/libraries.mk
+++ libraries/libraries.mk
@@ -87,11 +87,7 @@ LIB_STRICT_OBJECTS += $(LIBRARIES_PATH)/bsg_manycore_print_int_responder.o
 LIB_STRICT_OBJECTS += $(LIBRARIES_PATH)/bsg_manycore_memsys.o
 
 # Objects that should be compiled with debug flags
-# LIB_DEBUG_OBJECTS  += $(LIBRARIES_PATH)/bsg_manycore_config.o
-# LIB_DEBUG_OBJECTS  += $(LIBRARIES_PATH)/bsg_manycore_memsys.o
-# LIB_DEBUG_OBJECTS  += $(LIBRARIES_PATH)/bsg_manycore.o
-# LIB_DEBUG_OBJECTS  += $(LIBRARIES_PATH)/bsg_manycore_eva.o
-# LIB_DEBUG_OBJECTS  += $(LIBRARIES_PATH)/bsg_manycore_cuda.o
+LIB_DEBUG_OBJECTS  +=
 
 LIB_OBJECTS += $(patsubst %cpp,%o,$(LIB_CXXSOURCES))
 LIB_OBJECTS += $(patsubst %c,%o,$(LIB_CSOURCES))
