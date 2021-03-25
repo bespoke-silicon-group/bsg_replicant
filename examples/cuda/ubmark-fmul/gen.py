8f49dcf73e034375436fc153440841d750bcb89b
lc873 <lc873@cornell.edu>
2021-03-25 13:34:16 -0400

    [energy] fix float input for fmul

--- examples/cuda/ubmark-fmul/gen.py
+++ examples/cuda/ubmark-fmul/gen.py
@@ -1,10 +1,16 @@
 template = \
-"""asm volatile("fadd.s  %0, %1, %2"     : "+f"(C_tmp) : "f"(A_tmp),  "f"(B_tmp));
+    """asm volatile("fadd.s  %0, %1, %2"     : "+f"(tmp{0}) : "f"(tmp{1}),  "f"(tmp{2}));
 //------------------------------------
 """
 
+numReg   = 10
+regIndex = 0
 string = ""
 for i in range(255):
-  string += template.format()
+  rd  = regIndex
+  rs1 = (regIndex + 1) % numReg
+  rs2 = (regIndex + 2) % numReg
+  string += template.format(rd, rs1, rs2)
+  regIndex = (regIndex + 1) % numReg
 
 print(string)
