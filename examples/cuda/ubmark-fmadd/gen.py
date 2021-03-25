b8492d03a3f4398b6a37785d8d1c9121748303a6
lc873 <lc873@cornell.edu>
2021-03-25 13:55:36 -0400

    [energy] fix input for fmadd

--- examples/cuda/ubmark-fmadd/gen.py
+++ examples/cuda/ubmark-fmadd/gen.py
@@ -1,10 +1,17 @@
 template = \
-"""asm volatile("fadd.s  %0, %1, %2"     : "+f"(C_tmp) : "f"(A_tmp),  "f"(B_tmp));
+    """asm volatile("fmadd.s %0, %1, %2, %3" : "=f"(tmp{0}) : "f"(tmp{1}),  "f"(tmp{2}), "f"(tmp{3}));
 //------------------------------------
 """
 
+numReg   = 10
+regIndex = 0
 string = ""
 for i in range(255):
-  string += template.format()
+  rd  = regIndex
+  rs1 = (regIndex + 1) % numReg
+  rs2 = (regIndex + 2) % numReg
+  rs3 = (regIndex + 3) % numReg
+  string += template.format(rd, rs1, rs2, rs3)
+  regIndex = (regIndex + 1) % numReg
 
 print(string)
