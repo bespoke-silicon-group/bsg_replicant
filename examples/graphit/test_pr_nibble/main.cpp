e7f74e62bd59cbb7bb507f4c74903f6eb8bd49d6
Emily Furst <eafurst@cs.washington.edu>
2021-03-29 15:04:36 -0700

    working pr nibble test

--- examples/graphit/test_pr_nibble/main.cpp
+++ examples/graphit/test_pr_nibble/main.cpp
@@ -20,8 +20,8 @@ GlobalScalar<hb_mc_eva_t> p_dev;
 GlobalScalar<hb_mc_eva_t> old_rank_dev;
 GlobalScalar<hb_mc_eva_t> new_rank_dev;
 GlobalScalar<hb_mc_eva_t> out_degree_dev;
-GlobalScalar<float > alpha_dev;
-GlobalScalar<float > epsilon_dev;
+//GlobalScalar<float > alpha_dev;
+//GlobalScalar<float > epsilon_dev;
 
 #include "pr_host.hpp"
 
@@ -57,25 +57,25 @@ int launch(int argc, char ** argv){
   hammerblade::builtin_loadMicroCodeFromFile(ucode_path);
 
   std::cerr << "load graph" << std::endl;
-  //std::string graph_f = input.getCmdOption("-g");
-  std::string graph_f = "~/research/bladerunner6.0/graphit-new/test/graphs/darpa-eval/jhu.mtx"; 
+  std::string graph_f = input.getCmdOption("-g");
   edges = hammerblade::builtin_loadEdgesFromFileToHB (graph_f.c_str()); 
 
   std::cerr << "size of graph: " << std::endl;
   std::cerr << edges.num_nodes() << std::endl;
   std::cerr << edges.num_edges() << std::endl; 
-
   std::cerr << "init global scalars" << std::endl; 
+
   p_dev = GlobalScalar<hb_mc_eva_t>("p");
-  hammerblade::init_global_array<double>(hammerblade::builtin_getVerticesHB(edges), p_dev);
+
+  hammerblade::init_global_array<float>(hammerblade::builtin_getVerticesHB(edges), p_dev);
   old_rank_dev = GlobalScalar<hb_mc_eva_t>("old_rank");
-  hammerblade::init_global_array<double>(hammerblade::builtin_getVerticesHB(edges), old_rank_dev);
+  hammerblade::init_global_array<float>(hammerblade::builtin_getVerticesHB(edges), old_rank_dev);
   new_rank_dev = GlobalScalar<hb_mc_eva_t>("new_rank");
-  hammerblade::init_global_array<double>(hammerblade::builtin_getVerticesHB(edges), new_rank_dev);
+  hammerblade::init_global_array<float>(hammerblade::builtin_getVerticesHB(edges), new_rank_dev);
   out_degree_dev = GlobalScalar<hb_mc_eva_t>("out_degree");
   hammerblade::init_global_array<int32_t>(hammerblade::builtin_getVerticesHB(edges), out_degree_dev);
-  alpha_dev = GlobalScalar<float>("alpha");
-  epsilon_dev = GlobalScalar<float>("epsilon");
+  //alpha_dev = GlobalScalar<float>("alpha");
+  //epsilon_dev = GlobalScalar<float>("epsilon");
 
   std::cerr << "init locks" << std::endl;
   GlobalScalar<hb_mc_eva_t> glbl_locks = GlobalScalar<hb_mc_eva_t>("locks");
@@ -87,29 +87,27 @@ int launch(int argc, char ** argv){
   float alpha = ((float) 0.15) ;
   float epsilon = ((float) 1e-06) ;
   int start_vertex = ROOT;
-
-
   Vector<int32_t> frontier = Vector<int32_t>(hammerblade::builtin_getVerticesHB(edges));
   //Vector<int32_t> next_frontier = Vector<int32_t>(hammerblade::builtin_getVerticesHB(edges));
 
   std::vector<int32_t> hfrontier(edges.num_nodes(), 0);
-  std::vector<double> p(edges.num_nodes(), (double) 0.0);
-  std::vector<double> new_rank(edges.num_nodes(), (double) 0.0);
-  std::vector<double> old_rank(edges.num_nodes(), (double) 0.0);
+  std::vector<float> p(edges.num_nodes(), (float) 0.0);
+  std::vector<float> new_rank(edges.num_nodes(), (float) 0.0);
+  std::vector<float> old_rank(edges.num_nodes(), (float) 0.0);
   std::vector<int32_t> out_degs = edges.get_out_degrees();
 
   //compute up to current iter on host
   hfrontier[start_vertex] = 1;
-  new_rank[start_vertex] = (double) 1.0;
-  old_rank[start_vertex] = (double) 1.0;
+  new_rank[start_vertex] = (float) 1.0;
+  old_rank[start_vertex] = (float) 1.0;
   host_pr_calc(p, old_rank, new_rank, hfrontier, iter);
 
   frontier.copyToDevice(hfrontier.data(), hfrontier.size());
 
   //next_frontier.copyToDevice(zeros.data(), zeros.size());
-  hammerblade::write_global_buffer_dma<double>(p.data(), p_dev, p.size());  
-  hammerblade::write_global_buffer_dma<double>(old_rank.data(), old_rank_dev, old_rank.size());  
-  hammerblade::write_global_buffer_dma<double>(new_rank.data(), new_rank_dev, new_rank.size());  
+  hammerblade::write_global_buffer_dma<float>(p.data(), p_dev, p.size());  
+  hammerblade::write_global_buffer_dma<float>(old_rank.data(), old_rank_dev, old_rank.size());  
+  hammerblade::write_global_buffer_dma<float>(new_rank.data(), new_rank_dev, new_rank.size());  
   hammerblade::write_global_buffer_dma<int32_t>(out_degs.data(), out_degree_dev, out_degs.size());  
   hammerblade::write_global_buffer_dma<std::atomic<int>>(tmp_a, glbl_locks, NUM_LOCKS);
 
@@ -160,7 +158,7 @@ int launch(int argc, char ** argv){
     	device->enqueueJob("filter_frontier_where_call", hb_mc_dimension(X,Y),{frontier.getAddr(), edges.num_nodes(), edges.num_edges(), tag_c});
     	device->runJobs();
     	std::cerr << "swap arrays\n";
-    	hammerblade::swap_global_arrays<double>(new_rank_dev, old_rank_dev);
+    	hammerblade::swap_global_arrays<float>(new_rank_dev, old_rank_dev);
       f_sz = builtin_getVertexSetSizeHB(frontier, edges.num_nodes());
       std::cerr << "size of frontier after iteration " << iter << " : " << f_sz << std::endl;
       break;
@@ -175,7 +173,7 @@ int launch(int argc, char ** argv){
     	device->runJobs();
       tag_c++;
     	std::cerr << "swap arrays\n";
-   	hammerblade::swap_global_arrays<double>(new_rank_dev, old_rank_dev);
+   	hammerblade::swap_global_arrays<float>(new_rank_dev, old_rank_dev);
     	std::cerr << "create next frontier\n";
     	device->enqueueJob("filter_frontier_where_call", hb_mc_dimension(X,Y),{frontier.getAddr(), edges.num_nodes(), edges.num_edges(), tag_c});
     	device->runJobs();
@@ -196,7 +194,7 @@ int launch(int argc, char ** argv){
     	device->enqueueJob("filter_frontier_where_call", hb_mc_dimension(X,Y),{frontier.getAddr(), edges.num_nodes(), edges.num_edges(), tag_c});
     	device->runJobs();
     	std::cerr << "swap arrays\n";
-    	hammerblade::swap_global_arrays<double>(new_rank_dev, old_rank_dev);
+    	hammerblade::swap_global_arrays<float>(new_rank_dev, old_rank_dev);
       f_sz = builtin_getVertexSetSizeHB(frontier, edges.num_nodes());
       std::cerr << "size of frontier after iteration " << iter << " : " << f_sz << std::endl;
       break;
@@ -214,8 +212,8 @@ int launch(int argc, char ** argv){
   if(VERIFY) {
     ofstream ver_file;
     ver_file.open("./rank.txt");
-    double host_rank[edges.num_nodes()];
-    hammerblade::read_global_buffer_dma<double>(host_rank, old_rank_dev, edges.num_nodes());
+    float host_rank[edges.num_nodes()];
+    hammerblade::read_global_buffer_dma<float>(host_rank, old_rank_dev, edges.num_nodes());
     for(int i = 0; i < edges.num_nodes(); i++) {
       ver_file << host_rank[i] << std::endl;
     }
