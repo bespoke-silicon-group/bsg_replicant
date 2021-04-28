a26bb5b8a859ac63e50f2ef35ee20be0655746a1
Max Ruttenberg <mrutt@cs.washington.edu>
2021-04-28 08:03:31 -0700

    Fixes test_tracer

--- examples/cuda/test_tracer/main.cpp
+++ examples/cuda/test_tracer/main.cpp
@@ -36,7 +36,7 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <stdio.h>
-#include <cl_manycore_regression.h>
+#include <bsg_manycore_regression.h>
 
 #define ALLOC_NAME "default_allocator"
 
@@ -247,15 +247,4 @@ int kernel_tracer (int argc, char **argv) {
         return HB_MC_SUCCESS;
 }
 
-#ifdef VCS
-int vcs_main(int argc, char ** argv) {
-#else
-int main(int argc, char ** argv) {
-#endif
-        bsg_pr_test_info("test_tracer Regression Test \n");
-        int rc = kernel_tracer(argc, argv);
-        bsg_pr_test_pass_fail(rc == HB_MC_SUCCESS);
-        return rc;
-}
-
-
+declare_program_main("test_tracer", kernel_tracer);
