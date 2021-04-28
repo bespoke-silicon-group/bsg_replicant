a2d8e0bbee35cd7d5a2ef590f0111a492052f322
Max Ruttenberg <mrutt@cs.washington.edu>
2021-04-28 08:01:01 -0700

    Fixes test_host_memset

--- examples/cuda/test_host_memset/main.c
+++ examples/cuda/test_host_memset/main.c
@@ -36,7 +36,7 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <stdio.h>
-#include <cl_manycore_regression.h>
+#include <bsg_manycore_regression.h>
 
 #define ALLOC_NAME "default_allocator"
 #define TEST_BYTE 0xcd
@@ -172,15 +172,4 @@ int kernel_host_memset (int argc, char **argv) {
         return HB_MC_SUCCESS;
 }
 
-#ifdef VCS
-int vcs_main(int argc, char ** argv) {
-#else
-int main(int argc, char ** argv) {
-#endif
-        bsg_pr_test_info("test_host_memset Regression Test \n");
-        int rc = kernel_host_memset(argc, argv);
-        bsg_pr_test_pass_fail(rc == HB_MC_SUCCESS);
-        return rc;
-}
-
-
+declare_program_main("test_host_memset", kernel_host_memset);
