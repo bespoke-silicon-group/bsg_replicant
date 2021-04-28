d31b2d4fb1efdf971d61084d26c97644a4b23c64
Max Ruttenberg <mrutt@cs.washington.edu>
2021-04-28 07:58:41 -0700

    Fixes test_float_matrix_mul_shared_mem

--- examples/cuda/test_float_matrix_mul_shared_mem/main.c
+++ examples/cuda/test_float_matrix_mul_shared_mem/main.c
@@ -46,7 +46,7 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <stdio.h>
-#include <cl_manycore_regression.h>
+#include <bsg_manycore_regression.h>
 
 #define ALLOC_NAME "default_allocator"
 
@@ -278,16 +278,4 @@ int kernel_float_matrix_mul_shared_mem (int argc, char **argv) {
         }
         return HB_MC_SUCCESS;
 }
-
-#ifdef VCS
-int vcs_main(int argc, char ** argv) {
-#else
-int main(int argc, char ** argv) {
-#endif
-        bsg_pr_test_info("test_float_matrix_mul_shared_mem Regression Test \n");
-        int rc = kernel_float_matrix_mul_shared_mem(argc, argv);
-        bsg_pr_test_pass_fail(rc == HB_MC_SUCCESS);
-        return rc;
-}
-
-
+declare_program_main("test_float_matrix_mul_shared_mem", kernel_float_matrix_mul_shared_mem);
