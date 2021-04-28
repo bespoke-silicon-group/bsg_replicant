ae32fd0cecb6f7bd2308639617eea5c59e16d750
Max Ruttenberg <mrutt@cs.washington.edu>
2021-04-28 08:00:08 -0700

    Fixes test_float_vec_log

--- examples/cuda/test_float_vec_log/main.c
+++ examples/cuda/test_float_vec_log/main.c
@@ -45,7 +45,7 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <stdio.h>
-#include <cl_manycore_regression.h>
+#include <bsg_manycore_regression.h>
 
 #define ALLOC_NAME "default_allocator"
 
@@ -241,15 +241,4 @@ int kernel_float_vec_log (int argc, char **argv) {
         return HB_MC_SUCCESS;
 }
 
-#ifdef VCS
-int vcs_main(int argc, char ** argv) {
-#else
-int main(int argc, char ** argv) {
-#endif
-        bsg_pr_test_info("test_float_vec_log Regression Test\n");
-        int rc = kernel_float_vec_log(argc, argv);
-        bsg_pr_test_pass_fail(rc == HB_MC_SUCCESS);
-        return rc;
-}
-
-
+declare_program_main("test_float_vec_log", kernel_float_vec_log);
