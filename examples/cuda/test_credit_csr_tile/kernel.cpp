924c044eb29991dae3ed68b23c088013d8610a5d
Dustin Richmond <dustinar@uw.edu>
2021-06-10 14:59:32 -0700

    Use memory fence and split loop to help compiler

--- examples/cuda/test_credit_csr_tile/kernel.cpp
+++ examples/cuda/test_credit_csr_tile/kernel.cpp
@@ -10,9 +10,16 @@ int kernel_vec_add_parallel(float bsg_attr_remote * bsg_attr_noalias A, float bs
 
         asm volatile ("csrw 0xfc0, %[limit]" : : [limit] "r" (limit));
         for (int blk_i = 0; blk_i < nblocks; blk_i += 1) {
-                bsg_unroll(32)
-                for (int i = 0; i < 32; i += 1) {
-                        C[i + (blk_i * 32)] = A[i + (blk_i * 32)] + B[i + (blk_i * 32)];
+                register float a_temp[16], b_temp[16];
+                bsg_unroll(16)
+                for (int i = 0; i < 16; i += 1) {
+                        a_temp[i] = A[i + (blk_i * 16)];
+                        b_temp[i] = B[i + (blk_i * 16)];
+                }
+                __asm__ __volatile__ ("" : : : "memory");
+                bsg_unroll(16)
+                for (int i = 0; i < 16; i += 1) {
+                        C[i + (blk_i * 16)] =  a_temp[i] + b_temp[i];
                 }
         }
 
