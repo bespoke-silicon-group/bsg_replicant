d84e1ad31b0f5f21f82079a94bc1272c9708708b
Dustin Richmond <dustinar@uw.edu>
2021-06-30 15:12:32 -0700

    Replace one last main.exec.log

--- examples/spmd/loader.mk
+++ examples/spmd/loader.mk
@@ -110,7 +110,7 @@ include $(EXAMPLES_PATH)/execution.mk
 # Regression Flow
 ###############################################################################
 
-regression: main.exec.log
+regression: exec.log
 	@grep "BSG REGRESSION TEST .*PASSED.*" $< > /dev/null
 
 .DEFAULT_GOAL := help
