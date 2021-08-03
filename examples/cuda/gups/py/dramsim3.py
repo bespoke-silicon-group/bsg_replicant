ffa7e084cc7d24b9a789593bb1e0cdd3247d6dab
Max Ruttenberg <mrutt@cs.washington.edu>
2021-08-03 14:59:17 -0700

    script fix

--- examples/cuda/gups/py/dramsim3.py
+++ examples/cuda/gups/py/dramsim3.py
@@ -2,14 +2,14 @@ from dramsim3_utils import DRAMSim3Stats
 import sys
 import re
 import pandas as pd
-from gups_common import GUPSParameter
+from gups_common import GUPSParameters
 
 class GUPSDRAMSim3Stats(DRAMSim3Stats):
     def _subclass_init_add_group_by_fields(self):
         """
         Parse the experiment parameters from the filename
         """
-        self._parameters = GUPSParameter(self.filename)
+        self._parameters = GUPSParameters(self.filename)
         self._data = self._parameters.updateDataFrame(self._data)
         self._group_by_fields += self._parameters.parameters
         return
