180a58ca37d472d348551937627439f96bbdf9e3
Max Ruttenberg <mrutt@cs.washington.edu>
2021-08-16 06:22:51 -0700

    gitignore added

---
+++ examples/cuda/stride/py/dramsim3.py
@@ -0,0 +1,22 @@
+from dramsim3_utils import DRAMSim3Stats
+import sys
+import re
+import pandas as pd
+from stride_common import StrideParameters
+
+class StrideDRAMSim3Stats(DRAMSim3Stats):
+    def _subclass_init_add_group_by_fields(self):
+        """
+        Parse the experiment parameters from the filename
+        """
+        self._parameters = StrideParameters(self.filename)
+        self._data = self._parameters.updateDataFrame(self._data)
+        self._group_by_fields += self._parameters.parameters
+        return
+
+df = pd.DataFrame()
+for f in sys.argv[1:]:
+    df = df.append(StrideDRAMSim3Stats(f).diffed_data)
+
+df.to_csv('dramsim3.summary.csv')
+
