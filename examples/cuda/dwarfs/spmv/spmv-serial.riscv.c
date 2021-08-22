f41133a573aa01669ae03259327f9f8c910eab21
Max Ruttenberg <mrutt@cs.washington.edu>
2021-08-22 13:15:36 -0700

    fixup to spmv

--- examples/cuda/dwarfs/spmv/spmv-serial.riscv.c
+++ examples/cuda/dwarfs/spmv/spmv-serial.riscv.c
@@ -1,3 +1,5 @@
+#include "bsg_manycore.h"
+#include "sparse_matrix.h"
 #ifndef ROWS
 #error please define ROWS
 #endif
@@ -6,25 +8,24 @@
 #error "please define NNZ_PER_ROW"
 #endif
 
-int spmv(int     *rowptrs
-         , int   *cols
-         , float *nonzeros
+int spmv(sparse_matrix_t *spmatptr
          , float *vector_i
          , float *vector_o)
 {
+    sparse_matrix_t mat = *spmatptr;
     // for each row...
     for (int i = 0; i < ROWS; i++) {
-        int row_off = rowptrs[i];
+        int row_off = mat.mnr_off_ptr[i];
         float row_sum = 0.0;
         // for each non-zero in row
         for (int j = 0; j < NNZ_PER_ROW; j++) {
-            // fetch non-zero's column 
-            int k    = cols[row_off+j];
+            // fetch non-zero's column
+            int k    = mat.mnr_idx_ptr[row_off+j];
             // fetch non-zero's data
-            float d0 = nonzeros[row_off+j];
+            float d0 = mat.val_ptr[row_off+j];
             // fetch input vector's data
             float d1 = vector_i[k];
-            // fma
+            // fma            
             row_sum += d0*d1;
         }
         // update output
