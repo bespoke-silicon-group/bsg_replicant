703ba52938d562c4dbf127e689f801f2824384c2
Max Ruttenberg <mrutt@cs.washington.edu>
2021-09-15 16:51:52 -0700

    adds union vanilla pointers for easy casting

--- examples/cuda/dwarfs/include/common/sparse_matrix.h
+++ examples/cuda/dwarfs/include/common/sparse_matrix.h
@@ -9,11 +9,26 @@ extern "C" {
         int n_major;
         int n_minor;
         int n_non_zeros;
-        kernel_remote_int_ptr_t mjr_nnz_ptr;
-        kernel_remote_int_ptr_t mnr_off_ptr;
-        kernel_remote_int_ptr_t mnr_idx_ptr;
-        kernel_remote_float_ptr_t   val_ptr;
-        kernel_remote_int_ptr_t alg_priv_data;
+        union {
+            kernel_remote_int_ptr_t mjr_nnz_ptr;
+            kernel_int_ptr_t        mjr_nnz_ptr_vanilla;
+        };
+        union {
+            kernel_remote_int_ptr_t mnr_off_ptr;
+            kernel_int_ptr_t        mnr_off_ptr_vanilla;
+        };
+        union {
+            kernel_remote_int_ptr_t mnr_idx_ptr;
+            kernel_int_ptr_t        mnr_idx_ptr_vanilla;
+        };
+        union {
+            kernel_remote_float_ptr_t val_ptr;
+            kernel_float_ptr_t        val_ptr_vanilla;
+        };
+        union {
+            kernel_remote_int_ptr_t alg_priv_data;
+            kernel_int_ptr_t        alg_priv_data_vanilla;
+        };
     } sparse_matrix_t;
 
 #ifdef __cplusplus
