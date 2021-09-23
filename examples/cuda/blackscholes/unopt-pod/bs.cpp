d7ec671d3d91400ee67255572b325d31b35dac13
Dustin Richmond <dustinar@uw.edu>
2021-09-23 13:32:11 -0700

    Add unopt variants

--- examples/cuda/blackscholes/unopt-pod/bs.cpp
+++ examples/cuda/blackscholes/unopt-pod/bs.cpp
@@ -1,7 +1,7 @@
 #include <cmath>
 // Cumulative Normal Distribution Function
 // See Hull, Section 11.8, P.243-244
-#define inv_sqrt_2xPI 0.39894228040143270286f
+#define inv_sqrt_2xPI 0.39894228040143270286
 float CNDF (float InputX)
 {
     int sign;
@@ -17,7 +17,7 @@ float CNDF (float InputX)
     float xLocal_2, xLocal_3;
 
     // Check for negative value of InputX
-    if (InputX < 0.0f) {
+    if (InputX < 0.0) {
         InputX = -InputX;
         sign = 1;
     } else 
@@ -26,41 +26,41 @@ float CNDF (float InputX)
     xInput = InputX;
  
     // Compute NPrimeX term common to both four & six decimal accuracy calcs
-    expValues = expf(-0.5f * InputX * InputX);
+    expValues = exp(-0.5f * InputX * InputX);
     xNPrimeofX = expValues;
     xNPrimeofX = xNPrimeofX * inv_sqrt_2xPI;
 
-    xK2 = 0.2316419f * xInput;
-    xK2 = 1.0f + xK2;
-    xK2 = 1.0f / xK2;
+    xK2 = 0.2316419 * xInput;
+    xK2 = 1.0 + xK2;
+    xK2 = 1.0 / xK2;
     xK2_2 = xK2 * xK2;
     xK2_3 = xK2_2 * xK2;
     xK2_4 = xK2_3 * xK2;
     xK2_5 = xK2_4 * xK2;
     
-    xLocal_1 = xK2 * 0.319381530f;
-    xLocal_2 = xK2_2 * (-0.356563782f);
-    xLocal_3 = xK2_3 * 1.781477937f;
+    xLocal_1 = xK2 * 0.319381530;
+    xLocal_2 = xK2_2 * (-0.356563782);
+    xLocal_3 = xK2_3 * 1.781477937;
     xLocal_2 = xLocal_2 + xLocal_3;
-    xLocal_3 = xK2_4 * (-1.821255978f);
+    xLocal_3 = xK2_4 * (-1.821255978);
     xLocal_2 = xLocal_2 + xLocal_3;
-    xLocal_3 = xK2_5 * 1.330274429f;
+    xLocal_3 = xK2_5 * 1.330274429;
     xLocal_2 = xLocal_2 + xLocal_3;
 
     xLocal_1 = xLocal_2 + xLocal_1;
     xLocal   = xLocal_1 * xNPrimeofX;
-    xLocal   = 1.0f - xLocal;
+    xLocal   = 1.0 - xLocal;
 
     OutputX  = xLocal;
     
     if (sign) {
-        OutputX = 1.0f - OutputX;
+        OutputX = 1.0 - OutputX;
     }
     
     return OutputX;
 } 
 
-float BlkSchlsEqEuroNoDiv( float sptprice,
+__attribute__((noinline)) float BlkSchlsEqEuroNoDiv( float sptprice,
                             float strike, float rate, float volatility,
                             float time, int otype, float timet )
 {
@@ -97,12 +97,12 @@ float BlkSchlsEqEuroNoDiv( float sptprice,
     xSqrtTime = sqrt(xTime);
 
     // DR: Could compure this on the host...
-    logValues = logf( sptprice / strike );
+    logValues = log( sptprice / strike );
         
     xLogTerm = logValues;
     
     xPowerTerm = xVolatility * xVolatility;
-    xPowerTerm = xPowerTerm * 0.5f;
+    xPowerTerm = xPowerTerm * 0.5;
         
     xD1 = xRiskFreeRate + xPowerTerm;
     xD1 = xD1 * xTime;
@@ -118,12 +118,12 @@ float BlkSchlsEqEuroNoDiv( float sptprice,
     NofXd1 = CNDF( d1 );
     NofXd2 = CNDF( d2 );
 
-    FutureValueX = strike * ( expf( -(rate)*(time) ) );        
+    FutureValueX = strike * ( exp( -(rate)*(time) ) );        
     if (otype == 0) {            
         OptionPrice = (sptprice * NofXd1) - (FutureValueX * NofXd2);
     } else { 
-        NegNofXd1 = (1.0f - NofXd1);
-        NegNofXd2 = (1.0f - NofXd2);
+        NegNofXd1 = (1.0 - NofXd1);
+        NegNofXd2 = (1.0 - NofXd2);
         OptionPrice = (FutureValueX * NegNofXd2) - (sptprice * NegNofXd1);
     }
     
