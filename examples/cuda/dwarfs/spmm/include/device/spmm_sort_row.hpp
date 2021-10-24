7ff82fd385665acacd935e68f76ca86763a40cd6
Max Ruttenberg <mrutt@cs.washington.edu>
2021-10-23 19:11:09 -0700

    sort row not inlined

--- examples/cuda/dwarfs/spmm/include/device/spmm_sort_row.hpp
+++ examples/cuda/dwarfs/spmm/include/device/spmm_sort_row.hpp
@@ -1,12 +1,4 @@
 #pragma once
-#include <algorithm>
 #include "spmm.hpp"
 
-static void spmm_sort_row(int Ci)
-{
-    spmm_partial_t *partials = reinterpret_cast<spmm_partial_t*>(C_lcl.alg_priv_ptr[Ci]);
-    int n = C_lcl.mjr_nnz_ptr[Ci];
-    std::sort(partials, partials+n, [](const spmm_partial_t&l, const spmm_partial_t&r) {
-            return l.idx < r.idx;
-        });
-}
+void spmm_sort_row(int Ci);
