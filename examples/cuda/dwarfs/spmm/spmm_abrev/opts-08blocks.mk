d12c805fd8145a78f94d25f4c9484094895189b9
Max Ruttenberg <mrutt@cs.washington.edu>
2021-11-07 10:24:35 -0800

    fixup

--- examples/cuda/dwarfs/spmm/spmm_abrev/opts-08blocks.mk
+++ examples/cuda/dwarfs/spmm/spmm_abrev/opts-08blocks.mk
@@ -1,7 +1,7 @@
 # use dynamic work scheduling
 SPMM_RV				= spmm_dynamic.riscv.rvo
 # use hash table to solve row
-SPMM_SOLVE_ROW_RV		= spmm_solve_row_hash_table_coop.riscv.rvo
+SPMM_SOLVE_ROW_RV		= spmm_solve_row_hash_table.riscv.rvo
 # use sum tree to compute offsets
 SPMM_COMPUTE_OFFSETS_RV		= spmm_compute_offsets_sum_tree.riscv.rvo
 # use copy results
