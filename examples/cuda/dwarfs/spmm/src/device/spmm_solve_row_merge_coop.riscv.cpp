38ca912479403dca896bf42b0d0032d135afafd0
Max Ruttenberg <mrutt@cs.washington.edu>
2022-01-13 21:58:48 -0800

    cooperative merge list

--- examples/cuda/dwarfs/spmm/src/device/spmm_solve_row_merge_coop.riscv.cpp
+++ examples/cuda/dwarfs/spmm/src/device/spmm_solve_row_merge_coop.riscv.cpp
@@ -1,3 +1,4 @@
+//#define DEBUG
 #include <cstddef>
 #include "bsg_manycore.h"
 #include "bsg_tile_config_vars.h"
@@ -9,6 +10,96 @@
 #ifdef SPMM_PREFETCH
 #define PREFETCH 4
 #endif
+
+namespace tile {
+    using namespace list;
+    /**
+     * return true if this tile should be a solver
+     */
+    static inline int am_solver() {
+        return utils::is_cache_adjacent(utils::y());
+    }
+
+    /**
+     * return true if this tile should be a merger
+     */
+    static inline int am_merger() {
+        return utils::y() == 1
+            || utils::y() == bsg_tiles_Y-2;
+    }
+
+    /**
+     * return true if this tile is used for memory
+     */
+    static inline int am_memory() {
+        return 1;
+    }
+
+    /**
+     * sets x,y to the solver's coordinate for this tile
+     */
+    static inline void my_solver(int &x, int &y) {
+        x = utils::x();
+        if (utils::is_south_not_north(utils::y())) {
+            y = bsg_tiles_Y-1;
+        } else {
+            y = 0;
+        }
+    }
+
+    /**
+     * sets x,y to the merger's coordinate for this tile
+     */
+    static inline void my_merger(int &x, int &y) {
+        x = utils::x();
+        if (utils::is_south_not_north(utils::y())) {
+            y = bsg_tiles_Y-2;
+        } else {
+            y = 1;
+        }
+    }
+    
+    // /**
+    //  * synchronize merge operation
+    //  */
+    // static union {
+    //     volatile list_t*  solver_to_merge; // solver sets to signal merge should happen
+    //     volatile intptr_t merger_done; // solver clears, merger sets to signal merge done
+    // } merge;
+
+    // /**
+    //  * synchronize free operation
+    //  */
+    // static union {
+    //     volatile list_t*  merger_to_free; // merger sets to signal free should happen
+    //     volatile intptr_t solver_done; // merger clears, solver sets to signal free done
+    // } free_partials;
+
+    /**
+     * synchronize between merger and memories
+     */
+    static union {
+        volatile int memory_init_done; // memories set this when initialization is done
+        volatile int solver_exit_done; // solver sets this when row solve exit() is called
+    } solver = {0};
+
+    static constexpr int INIT_DONE = 0xCAFEBABE;
+    static constexpr int EXIT_DONE = 0xDEADFFFF;
+
+// assembly equivalent of the above; note stalls after lr's, and at least one branch
+// mispredict on exit path
+
+    static inline int wait_local_int_asm (volatile int *ptr, int cond)
+    {
+        int tmp; __asm__ __volatile__("2: lr.w %0, %1\n\t"
+                                      "beq %0, %2, 1f\n\t"
+                                      "lr.w.aq %0, %1\n\t"
+                                      "bne %0, %2, 2b\n\t"
+                                      "1:\n\t" : "=&r" (tmp) : "A" (*ptr), "r" (cond)); 
+        return tmp; 
+    }
+    
+}
 
 namespace solve_row_merge
 {
@@ -278,11 +369,11 @@ void spmm_solve_row(int Ai)
         
         spmm_partial_t *save_buffer = (spmm_partial_t*)spmm_malloc(sz);
         int nz = 0;
-        pr_dbg("solve_row: write back\n");
-        pr_dbg("&row_partials->head = %p\n", &row_partials.head);
+        //pr_dbg("solve_row: write back\n");
+        //pr_dbg("&row_partials->head = %p\n", &row_partials.head);
         while (!list_empty(&row_partials)) {
             partial_t *part = partial_from_node(list_front(&row_partials));
-            pr_dbg("part = %p\n", part);            
+            //pr_dbg("part = %p\n", part);            
             list_pop_front(&row_partials);
             save_buffer[nz] = part->iv;
             free_partial(part);
@@ -306,13 +397,46 @@ void spmm_solve_row(int Ai)
  */
 void spmm_solve_row_init()
 {
+    pr_dbg("initializing\n");
     using namespace solve_row_merge;
+    using namespace tile;
+
+    solver.memory_init_done = 0;
+    
     list_init(&free_tg);
     list_init(&free_offchip);
     list_init(&row_partials);
-    for (int i = 0; i < ARRAY_SIZE(__local_partial_pool); i++) {
-        list_append(&free_tg, &__local_partial_pool[i].tbl);
+
+    // initialize local memory pools
+    if (am_memory()) {
+        for (int i = 0; i < ARRAY_SIZE(__local_partial_pool); i++) {
+            partial_t *tgn = utils::tile_group_pointer(&__local_partial_pool[i]);
+            list_append(&free_tg, &tgn->tbl);
+        }
+        solver.memory_init_done = INIT_DONE;
     }
+
+    // gather free local memory pools
+    if (am_solver()) {
+        list_t free_pool;
+        list_init(&free_pool);
+        int y = utils::inner_neighbor_y_to(utils::y());
+        for (; y != -1; y = utils::inner_neighbor_y_to(y)) {
+            pr_dbg("waiting for memory at %d\n", y);
+            volatile int *memory_init_done = bsg_tile_group_remote_pointer(
+                utils::x()
+                ,y
+                ,&solver.memory_init_done
+                );
+            while (*memory_init_done != INIT_DONE);
+            list_t *free_tg_rmt = bsg_tile_group_remote_pointer(utils::x(), y, &free_tg);
+            list_extend(&free_tg, free_tg_rmt);
+        }
+    } else {
+        wait_local_int_asm(&solver.solver_exit_done, EXIT_DONE);
+        pr_dbg("released\n");
+    }
+    
     // do we just allocate a huge chunk here?
     return;
 }
@@ -322,6 +446,16 @@ void spmm_solve_row_init()
  */
 void spmm_solve_row_exit()
 {
+    using namespace tile;
+    pr_dbg("exiting\n");
+    if (am_solver()) {
+        int y = utils::inner_neighbor_y_to(utils::y());
+        for (; y != -1; y = utils::inner_neighbor_y_to(y)) {
+            volatile int* exit_done_rmt =
+                bsg_tile_group_remote_pointer(utils::x(), y, &solver.solver_exit_done);
+            *exit_done_rmt = EXIT_DONE;
+        }
+    }
     return;
 }
 
