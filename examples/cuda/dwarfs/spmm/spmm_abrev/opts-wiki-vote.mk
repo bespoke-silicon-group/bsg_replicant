1bdd98f0920bf37ab88b5c78c5561c620c4b64cd
Max Ruttenberg <mrutt@cs.washington.edu>
2022-01-18 18:45:14 -0800

    wiki vote opts

--- examples/cuda/dwarfs/spmm/spmm_abrev/opts-wiki-vote.mk
+++ examples/cuda/dwarfs/spmm/spmm_abrev/opts-wiki-vote.mk
@@ -1,8 +1,8 @@
 # use dynamic work scheduling
 SPMM_RV				= spmm_dynamic.riscv.rvo
 # use hash table to solve row
-SPMM_SOLVE_ROW_RV		= spmm_solve_row_hash_table
-SPMM_HASH_TABLE			= yes
+SPMM_SOLVE_ROW_RV		= spmm_solve_row_merge
+SPMM_HASH_TABLE			= no
 # use sum tree to compute offsets
 SPMM_COMPUTE_OFFSETS_RV		= spmm_compute_offsets_sum_tree
 # use copy results
@@ -10,7 +10,7 @@ SPMM_COPY_RESULTS_RV		= spmm_copy_results
 # use sort
 SPMM_SORT_ROW_RV		= spmm_sort_row_stdsort
 # use local memory
-SPMM_SOLVE_ROW_LOCAL_DATA_WORDS	= $(shell echo 256*3|bc)
+SPMM_SOLVE_ROW_LOCAL_DATA_WORDS	= $(shell echo 200*3|bc)
 # use prefetch
 SPMM_PREFETCH                   = yes
 # prefetch factor of 4
@@ -18,6 +18,6 @@ SPMM_PREFETCH_N			= 4
 # use an aligned hash table
 SPMM_ALIGNED_TABLE		= yes
 # do sort results
-SPMM_SKIP_SORTING		= no
+SPMM_SKIP_SORTING		= yes
 # tiles grab four work items at once
 SPMM_WORK_GRANULARITY		= 4
