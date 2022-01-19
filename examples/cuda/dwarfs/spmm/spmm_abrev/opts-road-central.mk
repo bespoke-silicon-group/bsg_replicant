2743be4368bbc865f0cbcde96b28c793bff0dd24
Max Ruttenberg <mrutt@cs.washington.edu>
2022-01-18 18:49:34 -0800

    fixup; everyone uses merge

--- examples/cuda/dwarfs/spmm/spmm_abrev/opts-road-central.mk
+++ examples/cuda/dwarfs/spmm/spmm_abrev/opts-road-central.mk
@@ -1,7 +1,7 @@
 # use dynamic work scheduling
 SPMM_RV				= spmm_dynamic.riscv.rvo
 # use hash table to solve row
-SPMM_SOLVE_ROW_RV		= spmm_solve_row_insertion_sort
+SPMM_SOLVE_ROW_RV		= spmm_solve_row_merge
 SPMM_HASH_TABLE			= no
 # use sum tree to compute offsets
 SPMM_COMPUTE_OFFSETS_RV		= spmm_compute_offsets_sum_tree
@@ -10,7 +10,7 @@ SPMM_COPY_RESULTS_RV		= spmm_copy_results
 # don't use sort
 SPMM_SORT_ROW_RV		= spmm_sort_row_stdsort
 # use local memory
-SPMM_SOLVE_ROW_LOCAL_DATA_WORDS	= 256
+SPMM_SOLVE_ROW_LOCAL_DATA_WORDS	= $(shell echo 256*3|bc)
 # use prefetch
 SPMM_PREFETCH                   = yes
 # prefetch factor of 4
