2381683492b927970f5db6b15530fb2a93aa6ff9
Dustin Richmond <dustinar@uw.edu>
2022-03-08 13:54:05 -0800

    Merge opt-pod, opt-single

--- examples/apps/blackscholes/opt-single/kernel.cpp
+++ examples/apps/blackscholes/opt-single/kernel.cpp
@@ -1,19 +1,23 @@
 #include <bsg_manycore.h>
 #include <bsg_set_tile_x_y.h>
-#include <bsg_tile_group_barrier.hpp>
+#include <bsg_cuda_lite_barrier.h>
 #include "bs.hpp"
-#include <string.h>
 
-extern "C"  int kernel_black_scholes(OptionData bsg_attr_remote * bsg_attr_noalias data,
-                                    float bsg_attr_remote * bsg_attr_noalias prices,
+extern "C" int kernel_black_scholes(OptionData bsg_attr_remote * bsg_attr_noalias data,
+                                    float bsg_attr_remote * bsg_attr_noalias puts,
+                                    float bsg_attr_remote * bsg_attr_noalias calls,
                                     int elems){
-	int grid_idx = (__bsg_tile_group_id_y * __bsg_grid_dim_x + __bsg_tile_group_id_x);
+	int grid_idx = (bsg_tiles_X * __bsg_y + __bsg_x);
         int start = grid_idx * elems;
+        bsg_barrier_hw_tile_group_init();
+        bsg_barrier_hw_tile_group_sync();
         bsg_cuda_print_stat_kernel_start();
         //bsg_fence();
-#define CHUNK_SIZE 4
+        // Use chunk size to load a cache line
+#define CHUNK_SIZE 2
         OptionData _data[CHUNK_SIZE];
-        float _prices[CHUNK_SIZE];
+        float _puts[CHUNK_SIZE];
+        float _calls[CHUNK_SIZE];
 
         for(int i = start; i < (start + elems); i += CHUNK_SIZE){
 
@@ -22,15 +26,17 @@ extern "C"  int kernel_black_scholes(OptionData bsg_attr_remote * bsg_attr_noali
                 }
 
                 for(int ci = 0; ci < CHUNK_SIZE; ++ci){
-                        _prices[ci] = BlkSchlsEqEuroNoDiv(_data[ci].s, _data[ci].strike,
-                                                          _data[ci].r, _data[ci].v, _data[ci].t,
-                                                          _data[ci].OptionType, 0);
+                        BlkSchlsEqEuroNoDiv(_data[ci].s, _data[ci].strike,
+                                            _data[ci].r, _data[ci].v, _data[ci].t,
+                                            _puts[ci], _calls[ci], 0);
                 }
                 for(int ci = 0; ci < CHUNK_SIZE; ++ci){
-                        prices[i + ci] = _prices[ci];
+                        puts[i + ci] = _puts[ci];
+                        calls[i + ci] = _calls[ci];
                 }
         }
-
         bsg_cuda_print_stat_kernel_end();
+        bsg_fence();
+        bsg_barrier_hw_tile_group_sync();
         return 0;
 }
