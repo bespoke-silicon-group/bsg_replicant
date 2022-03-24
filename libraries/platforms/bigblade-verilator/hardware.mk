605f824f67c3f8b38275e1743773fe5a33aacc28
Dustin Richmond <dustinar@uw.edu>
2022-03-24 15:59:28 -0700

    Move Stat Snoop from AWS to generic hardware

--- libraries/platforms/bigblade-verilator/hardware.mk
+++ libraries/platforms/bigblade-verilator/hardware.mk
@@ -104,8 +104,7 @@ VSOURCES += $(BSG_MANYCORE_DIR)/testbenches/common/v/instr_trace.v
 VSOURCES += $(BSG_MANYCORE_DIR)/testbenches/common/v/vanilla_core_trace.v
 VSOURCES += $(BSG_MANYCORE_DIR)/testbenches/common/v/vanilla_core_profiler.v
 VSOURCES += $(BSG_MANYCORE_DIR)/testbenches/common/v/vanilla_core_pc_histogram.v
-# We reuse the print_stat_snoop module from aws-vcs
-VSOURCES += $(LIBRARIES_PATH)/platforms/aws-vcs/hardware/bsg_print_stat_snoop.v
+VSOURCES += $(HARDWARE_PATH)/bsg_print_stat_snoop.v
 
 VSOURCES += $(BSG_MANYCORE_DIR)/testbenches/common/v/router_profiler.v
 VSOURCES += $(BSG_MANYCORE_DIR)/testbenches/common/v/remote_load_trace.v
