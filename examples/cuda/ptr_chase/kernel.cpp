650dd5e0cd2facfdcc46fff838f06b1e9e229d2c
Dustin Richmond <dustinar@uw.edu>
2022-05-10 14:24:06 -0700

    Increase DRAM size to 24, add comments

--- examples/cuda/ptr_chase/kernel.cpp
+++ examples/cuda/ptr_chase/kernel.cpp
@@ -9,12 +9,13 @@
 extern "C" int kernel_dram_pointer_chase(unsigned int volatile ** ptrs, int stride, unsigned int niters){
         volatile unsigned int * cur[LATENCY_WORK];
         int start = 1 + __bsg_id * (stride * LATENCY_WORK);
+
+        // LATENCY_WORK is how much we will unroll the inner loop to hide latency
+        // This loop sets the start point for each work item.
         bsg_unroll(100)
         for (int w = 0; w < LATENCY_WORK; ++w){
                 cur[w] = ptrs[start + w];
         }
-
-
 
         bsg_barrier_hw_tile_group_init();
         // Start profiling
@@ -23,6 +24,8 @@ extern "C" int kernel_dram_pointer_chase(unsigned int volatile ** ptrs, int stri
         bsg_cuda_print_stat_kernel_start();
 
         for(int iter = 0; iter < niters; ++iter){
+                // Unroll to amortize loop overhead
+                // Divide by LATENCY_WORK so we don't overflow the icache
                 bsg_unroll(128)
                 for(int unroll = 0; unroll < 128/LATENCY_WORK; ++unroll){
                         bsg_unroll(100)
