e068eb35d77471fc7795f433bd47bc2543694194
Paul Gao <gaozihou@uw.edu>
2022-07-21 12:11:23 -0700

    print test name

--- libraries/platforms/bigblade-fpga/bsg_manycore_regression_platform.h
+++ libraries/platforms/bigblade-fpga/bsg_manycore_regression_platform.h
@@ -14,12 +14,12 @@ extern "C" {
 //    }
 //    declare_program_main("The name of your test", MyMain)
 //
-#define declare_program_main(test_name, name)           \
-    int main(int argc, char *argv[]) {                  \
-        bsg_pr_test_info("Regression Test: %s\n");      \
-        int rc = name(argc, argv);                      \
-        bsg_pr_test_pass_fail(rc == HB_MC_SUCCESS);     \
-        return rc;                                      \
+#define declare_program_main(test_name, name)                  \
+    int main(int argc, char *argv[]) {                         \
+        bsg_pr_test_info("Regression Test: %s\n", test_name);  \
+        int rc = name(argc, argv);                             \
+        bsg_pr_test_pass_fail(rc == HB_MC_SUCCESS);            \
+        return rc;                                             \
     }
 
 
