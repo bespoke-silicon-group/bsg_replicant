49792a47cec6176597dc9647f08cd5ce0504b9f8
Paul Gao <gaozihou@uw.edu>
2022-07-22 04:12:40 -0700

    fix compilation

--- libraries/platforms/bigblade-fpga/compilation.mk
+++ libraries/platforms/bigblade-fpga/compilation.mk
@@ -36,16 +36,24 @@ NC=\033[0m
 # set by the Makefile that includes this makefile..
 # 
 
-.PRECIOUS: %.o
+DEFINES    += -DVCS
+INCLUDES   += -I$(LIBRARIES_PATH)
+INCLUDES   += -I$(BSG_PLATFORM_PATH)
+
+LDFLAGS    += -lstdc++ -lc -L$(BSG_PLATFORM_PATH)
+CXXFLAGS   += $(DEFINES) -fPIC
+CFLAGS     += $(DEFINES) -fPIC
 
 # each regression target needs to build its .o from a .c and .h of the
 # same name
-%.o: %.c %.h
-	$(CC) -c -o $@ $< $(INCLUDES) $(CFLAGS) $(CDEFINES) -DBSG_TEST_NAME=$(patsubst %.c,%,$<) 
+%.o: %.c
+	$(CC) -c -o $@ $< $(INCLUDES) $(CFLAGS) $(CDEFINES)
 
 # ... or a .cpp and .hpp of the same name
-%.o: %.cpp %.hpp
-	$(CXX) -c -o $@ $< $(INCLUDES) $(CXXFLAGS) $(CXXDEFINES) -DBSG_TEST_NAME=$(patsubst %.cpp,%,$<) 
+%.o: %.cpp
+	$(CXX) -c -o $@ $< $(INCLUDES) $(CXXFLAGS) $(CXXDEFINES)
+
+.PRECIOUS: %.o
 
 .PHONY: platform.compilation.clean
 platform.compilation.clean:
