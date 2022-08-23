d95d60fb39625d2e795c06d3f03c0cf54af8d160
Zichao Yue <zy383@zhang-21.ece.cornell.edu>
2022-08-22 20:15:13 -0400

    Update main_single.cpp to match the kernel file

--- examples/cuda/bfs/main_singlepod.cpp
+++ examples/cuda/bfs/main_singlepod.cpp
@@ -35,6 +35,8 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 #include <stdio.h>
+#include <fstream>
+#include <iostream>
 #include <cmath>
 #include <bsg_manycore_regression.h>
 #include "HammerBlade.hpp"
@@ -54,63 +56,109 @@ HammerBlade::Ptr HB;
 
 int Main(int argc, char *argv[])
 {
+    //std::cout<<"===================================enter host=============================="<<std::endl;
     CL cl;
     cl.parse(argc, argv);
-
-    WGraph g_csr = WGraph::FromCSR("soc-Pokec","/work/global/zy383/Bladerunner6.4.0/bsg_replicant/examples/cuda/bfs/inputs/CSRfile/pokec/");
-    WGraph g_csc = WGraph::FromCSR("soc-Pokec","/work/global/zy383/Bladerunner6.4.0/bsg_replicant/examples/cuda/bfs/inputs/CSCfile/pokec/");
-    //WGraph g_csr = WGraph::FromCSR("lgc_csr_float32","/work/global/zy383/Bladerunner6.4.0/bsg_replicant/examples/cuda/bfs/inputs/CSRfile/lgc_ista/");
-    //WGraph g_csc = WGraph::FromCSR("lgc_csr_float32","/work/global/zy383/Bladerunner6.4.0/bsg_replicant/examples/cuda/bfs/inputs/CSCfile/lgc_ista/");
-    //WGraph g_csr = WGraph::FromGraph500Data(Graph500Data::FromFile(cl.input_graph_path()));
-    //WGraph g_csc = WGraph::FromGraph500Data(Graph500Data::FromFile(cl.input_graph_path()),true);
-    //WGraph g_csr = WGraph::FromCSR("wiki-Vote","/work/shared/users/phd/zy383/HB_Cosim/Bladerunner_6.2.0/bsg_bladerunner/bsg_replicant/examples/cuda/bfs/inputs/CSRfile/wiki-Vote/");
-    //WGraph g_csc = WGraph::C2SR("wiki-Vote","/work/shared/users/phd/zy383/HB_Cosim/Bladerunner_6.2.0/bsg_bladerunner/bsg_replicant/examples/cuda/bfs/inputs/CSCfile/wiki-Vote/");
-    
-    int iter = cl.bfs_iteration();
-
-    std::vector<SparsePushBFS> stats = SparsePushBFS::RunBFS(g_csr, cl.bfs_root(), cl.bfs_iteration(), false);
-
-    // load application
     HB = HammerBlade::Get();
     HB->load_application(cl.binary_path());
+    int iter = cl.bfs_iteration();
+    std::string input_graph_path = cl.input_graph_path();
+    std::string input_graph_name = cl.graph_type();
+    
+    //int pod_ite = cl.bfs_podnum();
+    //std::cout<<"====================ite ID is "<<iter<<"======================="<<std::endl;
+    int pod_ite = cl.pod_id();
+    //int num_pods = 64;
+    WGraph g = WGraph::FromCSR(input_graph_name,input_graph_path+"CSR/");
+    //WGraph g = WGraph::FromCSR("ljournal-2008","/work/shared/users/phd/zy383/HB_Cosim/Bladerunner_6.2.0/bsg_bladerunner/bsg_replicant/examples/cuda/bfs/inputs/CSRfile/livejournal/");
+    //SparsePushBFS stats = SparsePushBFS::RunBFS_single(g, cl.bfs_root(), cl.bfs_iteration(), false);
+    // load application
 
-    //decide the edge traversal direction
-    //float frontier_density = stats[iter].frontier_in().size()/g_csc.num_nodes();
-    //bsg_pr_info("Frontier density is %d, frontier size is %d, num of node is %d \n",frontier_density,(stats[iter].frontier_in()).size(), g_csc.num_nodes());
+    std::shared_ptr<WGraph> wgptr = std::shared_ptr<WGraph>(new WGraph(g));
+    std::set<int> frontier = {cl.bfs_root()};
+    std::set<int> visited = {cl.bfs_root()};
+    SparsePushBFS bfs = SparsePushBFS(wgptr, frontier, visited); 
+    bfs.run(iter+1);
+    
+    float frontier_size = bfs.frontier_in().size();
+    //std::cout<<"==================frontier_in size is "<<frontier_size<<"========================"<<std::endl;
+    int num_nodes = g.num_nodes();
+    float frontier_density = frontier_size/(float)num_nodes;
+    int direction;//edge traversal direction, 0 for pull and 1 for push
+    direction = (frontier_density>0.1) ? 0:1;
+
+    //compute frontier length in bits
+    //int frontier_b = direction == 0 ? num_nodes : frontier_size * 32;
+    //if frontier length smaller than 521KB, cache is warm after frontier transmission
+    int cachewarm = 0;
+    
+    const std::set<int>& frontier_out_host = bfs.frontier_out();
+    std::cout<<"\n =========================host out frontier size "<<frontier_out_host.size()<<"=======================\n"<<std::endl;
+    std::cout<<"=========================host in frontier size "<<frontier_size<<"=======================\n"<<std::endl;
+    //std::cout<<"=========================host visited io size "<<bfs.visited_in().size()<<"======================="<<std::endl;
+    
+    WGraph g_csr = WGraph::FromCSR(input_graph_name,input_graph_path+"CSC/",pod_ite,1);
+    WGraph g_csc = WGraph::FromCSC(input_graph_name,input_graph_path+"CSR/",pod_ite,1);
+    
     BFSGraph bfsg_csc(g_csc);
     BFSGraph bfsg_csr(g_csr);
-    float frontier_size = stats[iter].frontier_in().size();
-    float num_nodes = bfsg_csr.graph().num_nodes();
-    float frontier_density = frontier_size/num_nodes;
-    std::cout<<"density: "<<frontier_density<<"frontier size: "<<frontier_size<<"num_nodes:"<<num_nodes<<std::endl;
-    //bsg_pr_info("Frontier density is %s, frontier size is %s, num of node is %s \n",std::to_string(frontier_density),std::to_string(frontier_size), std::to_string(num_nodes));
-    BFSSparseSet frontier_in_sparse(stats[iter].frontier_in(), bfsg_csr.graph().num_nodes());
-    BFSDenseSet  frontier_in_dense(stats[iter].frontier_in(), bfsg_csr.graph().num_nodes());
-    BFSDenseSet  frontier_out(std::set<int>(),    bfsg_csr.graph().num_nodes());
-    BFSDenseSet  visited_io(stats[iter].visited_in(),   bfsg_csr.graph().num_nodes());
-    int direction;//edge traversal direction, 0 for pull and 1 for push
+    //std::cout<<"=================================== graph fromated! =============================="<<std::endl;
+    BFSSparseSet frontier_in_sparse(bfs.frontier_in(), num_nodes);
+    
+    //build bit vector for dense frontier in
+    int frontier_bit_len = (num_nodes+31)/32;
+    int frontier_bvector[frontier_bit_len];
+    for(int i=0; i<frontier_bit_len; i++){
+      frontier_bvector[i] = 0;
+    }
+    for (int m: bfs.frontier_in()){
+      frontier_bvector[m/32] = frontier_bvector[m/32] | (1<<(m%32));
+    }
+    
+    
+    
+    //TODO RECONSTRUCT VISITED VECTOR AND OUTPUT FRONTIER
+    std::set<int> visited_in = bfs.visited_in();
+    
+    
+    int visited_bit_len = (num_nodes + 31)/32;
+    BFSDenseSet   frontier_out_sparse(std::set<int>(),    num_nodes);
+    BFSDenseSet   frontier_out_dense(std::set<int>(),     visited_bit_len);
+    
+    //build bit vector for visited
+    int visited_bvector[visited_bit_len];
+    for(int i=0; i<visited_bit_len; i++){
+      visited_bvector[i] = 0;
+    }
+    for (int m: visited_in){
+      visited_bvector[m/32] = visited_bvector[m/32] | (1<<(m%32));
+    }
+    //BFSDenseSet  visited_io(visited_bvector,   visited_size);
+    
     hammerblade::host::HammerBlade::Ptr _hb(hammerblade::host::HammerBlade::Get());
     kernel_int_ptr_t dircetion_hb = _hb->alloc(sizeof(int));
-    
-    if (frontier_density>0.1){
-        direction = 0;
-    }
-    else{
-        direction = 1;
-    }
     _hb->push_write(dircetion_hb, &direction,sizeof(int));
+    
+    kernel_int_ptr_t len_hb = _hb->alloc(sizeof(int));
+    _hb->push_write(len_hb, &num_nodes,sizeof(int));
 
-    
-    
-    
+    kernel_int_ptr_t cachewarm_hb = _hb->alloc(sizeof(int));
+    _hb->push_write(cachewarm_hb, &cachewarm,sizeof(int));
+
+    kernel_int_ptr_t visited_hb = _hb->alloc(sizeof(int)*visited_bit_len);
+    _hb->push_write(visited_hb, &visited_bvector[0],sizeof(int)*visited_bit_len);
+
+    kernel_int_ptr_t frontier_in_dense_hb = _hb->alloc(sizeof(int)*frontier_bit_len);
+    _hb->push_write(frontier_in_dense_hb, &frontier_bvector[0],sizeof(int)*frontier_bit_len);
 
     bfsg_csr.formatOnDevice(); 
     bfsg_csc.formatOnDevice();
-    frontier_in_dense.formatOnDevice();
+    //frontier_in_dense.formatOnDevice();
     frontier_in_sparse.formatOnDevice();
-    frontier_out.formatOnDevice();
-    visited_io.formatOnDevice();
-
+    frontier_out_dense.formatOnDevice();
+    frontier_out_sparse.formatOnDevice();
+    //visited_io.formatOnDevice();
+    //std::cout<<"===================================prepare complete!=============================="<<std::endl;
     // sync writes
     HB->sync_write();
     bsg_pr_info("BFS iteration %d on %s graph with %d nodes and %d edges starting from root %d\n",
@@ -119,40 +167,50 @@ int Main(int argc, char *argv[])
                 bfsg_csr.num_nodes(),
                 bfsg_csr.num_edges(),
                 cl.bfs_root());
-
     bsg_pr_info("Launching BFS with %d groups of shape (x=%d,y=%d)\n", cl.groups(), cl.tgx(), cl.tgy());
     HB->push_job(Dim(cl.groups(),1), Dim(cl.tgx(),cl.tgy()),
-                 "bfs", bfsg_csr.kgraph_dev(),bfsg_csc.kgraph_dev(), frontier_in_sparse.dev(), frontier_in_dense.dev(), frontier_out.dev(), visited_io.dev(),dircetion_hb);
+             "bfs", bfsg_csr.kgraph_dev(),bfsg_csc.kgraph_dev(), frontier_in_sparse.dev(), frontier_in_dense_hb, frontier_out_sparse.dev(), frontier_out_dense.dev(), visited_hb,dircetion_hb,len_hb,cachewarm_hb);
     HB->exec();
-
     // read output
-    frontier_out.updateFromDevice();
-    visited_io.updateFromDevice();
+    //frontier_out_dense.updateFromDevice();
+    frontier_out_sparse.updateFromDevice();
+    //visited_io.updateFromDevice();
+    int visited_dev;
+    _hb->push_read(len_hb, &visited_dev, sizeof(int));
     HB->sync_read();
-
-    std::set<int> frontier_out_kernel = frontier_out.setAfterUpdate();
-    const std::set<int>& frontier_out_host = stats[iter].frontier_out();
-
-    // check sets are equal
-    // check that host contains kernel
+    //TODO RETREIVE PUSH OUTPUT
+    //std::set<int> frontier_out_kernel_de = frontier_out_dense.setAfterUpdate();
+    std::set<int> frontier_out_kernel_sp = frontier_out_sparse.setAfterUpdate(visited_dev);
+    
+    
+    std::cout<<"===================================host out length is "<<frontier_out_host.size()<<"kernel out length is "<<visited_dev<<"=============================="<<std::endl;
     bool equals = true;
     for (int m : frontier_out_host) {
-        auto it = frontier_out_kernel.find(m);
-        if (it == frontier_out_kernel.end()) {
-            bsg_pr_err("Found %d in host result but not kernel\n", m);
+        auto it = frontier_out_kernel_sp.find(m);
+        if (it == frontier_out_kernel_sp.end()) {
+            bsg_pr_err("Found %d in host result but not kernel in iteration %d\n", m,iter);
             equals = false;
         }
+        //else{
+        //    bsg_pr_err("++++Found %d in both host and kernel in iteration %d\n", m,iter);
+        //}
     }
-    // check that kernel contains host
-    for (int m : frontier_out_kernel) {
+    for (int m : frontier_out_kernel_sp) {
         auto it = frontier_out_host.find(m);
         if (it == frontier_out_host.end()) {
-            bsg_pr_err("Found %d in kernel result but not host\n", m);
+            bsg_pr_err("Found %d in kernel result but not host in iteration %d\n", m,iter);
             equals = false;
         }
+        //else{
+        //    bsg_pr_err("++++Found %d in both host and kernel in iteration %d\n", m,iter);
+        //}
     }
 
-    stats[iter].dump("bfs_stats.txt");
+    //stats.dump("bfs_stats.txt");
+    std::ofstream outputfile;
+    outputfile.open("out_put_lenth.txt");
+    outputfile<<frontier_out_kernel_sp.size()<<std::endl;
+    outputfile.close();
     HB->close();
     return equals ? HB_MC_SUCCESS : HB_MC_FAIL;
 }
