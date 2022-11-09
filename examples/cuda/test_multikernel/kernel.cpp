2fa5ce7b56ec42202e0e5154c9d6e4ea012193b7
Dan Petrisko <petrisko@cs.washington.edu>
2022-11-08 19:55:54 -0800

    Cleanup

--- examples/cuda/test_multikernel/kernel.cpp
+++ examples/cuda/test_multikernel/kernel.cpp
@@ -5,55 +5,35 @@
 
 #include "bsg_tile_group_barrier.hpp"
 
-extern "C" __attribute__ ((noinline)) 
-int kernel0(int *buffer, int N, int *sync, int K) {
-
-  bsg_printf("Hello from kernel 0: Tile X: %x Tile Y: %x\n", __bsg_x, __bsg_y);
-
-  for (int i = 0; i < N; i++) {
-      buffer[0*N+i] += 100;
-      bsg_printf("kernel 0 Process[%d]: %d\n", i, buffer[0*N+i]);
-  } 
-
-  return 0;
+#define MAKE_FUNCTION_NAME(x) kernel##x
+#define FUNCTION_NAME(x) \
+  extern "C" __attribute__ ((noinline))                                               \
+  int MAKE_FUNCTION_NAME(x)(int *buffer, int N, int *sync, int K) {                   \
+  bsg_printf("Hello from kernel %d -- Tile X: %d Tile Y: %d\n", x, __bsg_x, __bsg_y); \
+                                                                                      \
+  for (int i = 0; i < N; i++) {                                                       \
+      buffer[x*N+i] += 100*(x+1);                                                     \
+      bsg_printf("kernel %d Process[%d]: %d\n", x, i, buffer[x*N+i]);                 \
+  }                                                                                   \
+                                                                                      \
+  return 0;                                                                           \
 }
 
-extern "C" __attribute__ ((noinline)) 
-int kernel1(int *buffer, int N, int *sync, int K) {
-
-  bsg_printf("Hello from kernel 1: Tile X: %x Tile Y: %x\n", __bsg_x, __bsg_y);
-
-  for (int i = 0; i < N; i++) {
-      buffer[1*N+i] += 200;
-      bsg_printf("kernel 1 Process[%d]: %d\n", i, buffer[1*N+i]);
-  } 
-
-  return 0;
-}
-
-extern "C" __attribute__ ((noinline)) 
-int kernel2(int *buffer, int N, int *sync, int K) {
-
-  bsg_printf("Hello from kernel 2: Tile X: %x Tile Y: %x\n", __bsg_x, __bsg_y);
-
-  for (int i = 0; i < N; i++) {
-      buffer[2*N+i] += 300;
-      bsg_printf("kernel 2 Process[%d]: %d\n", i, buffer[2*N+i]);
-  } 
-
-  return 0;
-}
-
-extern "C" __attribute__ ((noinline)) 
-int kernel3(int *buffer, int N, int *sync, int K) {
-
-  bsg_printf("Hello from kernel 3: Tile X: %x Tile Y: %x\n", __bsg_x, __bsg_y);
-
-  for (int i = 0; i < N; i++) {
-      buffer[3*N+i] += 400;
-      bsg_printf("kernel 3 Process[%d]: %d\n", i, buffer[3*N+i]);
-  } 
-
-  return 0;
-}
+FUNCTION_NAME(0)
+FUNCTION_NAME(1)
+FUNCTION_NAME(2)
+FUNCTION_NAME(3)
+FUNCTION_NAME(4)
+FUNCTION_NAME(5)
+FUNCTION_NAME(6)
+FUNCTION_NAME(7)
+FUNCTION_NAME(8)
+FUNCTION_NAME(9)
+FUNCTION_NAME(10)
+FUNCTION_NAME(11)
+FUNCTION_NAME(12)
+FUNCTION_NAME(13)
+FUNCTION_NAME(14)
+FUNCTION_NAME(15)
+FUNCTION_NAME(16)
 
