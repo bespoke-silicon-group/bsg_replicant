20d714ba450f1c7d917e7b8fdc0a0ac02128f2e4
Dan Petrisko <petrisko@cs.washington.edu>
2023-08-10 17:02:24 -0700

    Test importing headers for zynqparrot

--- libraries/platforms/zynqparrot-fpga/bsg_manycore_platform.cpp
+++ libraries/platforms/zynqparrot-fpga/bsg_manycore_platform.cpp
@@ -13,12 +13,6 @@
 // Header Definitions
 /////////////////////////////////////////////
 
-#define GP0_ENABLE
-#define GP0_ADDR_BASE 0x400000000
-#define GP0_ADDR_WIDTH 10
-#define GP0_DATA_WIDTH 32
-#define GP0_HIER_BASE ""
-
 #define _BSG_PRINTING_H
 #include <bsg_zynq_pl.h>
 #include <bsg_tag_bitbang.h>
@@ -26,35 +20,7 @@
 #include <bsg_manycore_request_packet.h>
 #include <bsg_manycore_response_packet.h>
 
-
-// GP0 Read Memory Map
-#define GP0_RD_CSR_SYS_RESETN    (GP0_ADDR_BASE                   )
-#define GP0_RD_CSR_TAG_BITBANG   (GP0_RD_CSR_SYS_RESETN    + 0x4  )
-#define GP0_RD_CSR_DRAM_INITED   (GP0_RD_CSR_TAG_BITBANG   + 0x4  )
-#define GP0_RD_CSR_DRAM_BASE     (GP0_RD_CSR_DRAM_INITED   + 0x4  )
-#define GP0_RD_CSR_ROM_ADDR      (GP0_RD_CSR_DRAM_BASE     + 0x4  )
-#define GP0_RD_MC_REQ_FIFO_DATA  (GP0_RD_CSR_ROM_ADDR      + 0x4  )
-#define GP0_RD_MC_RSP_FIFO_DATA  (GP0_RD_MC_REQ_FIFO_DATA  + 0x4  )
-#define GP0_RD_MC_REQ_FIFO_CTR   (GP0_RD_MC_RSP_FIFO_DATA  + 0x4  )
-#define GP0_RD_MC_RSP_FIFO_CTR   (GP0_RD_MC_REQ_FIFO_CTR   + 0x4  )
-#define GP0_RD_EP_REQ_FIFO_CTR   (GP0_RD_MC_RSP_FIFO_CTR   + 0x4  )
-#define GP0_RD_EP_RSP_FIFO_CTR   (GP0_RD_EP_REQ_FIFO_CTR   + 0x4  )
-#define GP0_RD_CREDIT_COUNT      (GP0_RD_EP_RSP_FIFO_CTR   + 0x4  )
-#define GP0_RD_ROM_DATA          (GP0_RD_CREDIT_COUNT      + 0x4  )
-
-// GP0 Write Memory Map
-#define GP0_WR_CSR_SYS_RESETN     GP0_RD_CSR_SYS_RESETN
-#define GP0_WR_CSR_TAG_BITBANG    GP0_RD_CSR_TAG_BITBANG
-#define GP0_WR_CSR_DRAM_INITED    GP0_RD_CSR_DRAM_INITED
-#define GP0_WR_CSR_DRAM_BASE      GP0_RD_CSR_DRAM_BASE
-#define GP0_WR_CSR_ROM_ADDR       GP0_RD_CSR_ROM_ADDR
-#define GP0_WR_EP_REQ_FIFO_DATA  (GP0_WR_CSR_ROM_ADDR + 0x4)
-#define GP0_WR_EP_RSP_FIFO_DATA  (GP0_WR_EP_REQ_FIFO_DATA + 0x4)
-
-#define TAG_NUM_CLIENTS 16
-#define TAG_MAX_LEN 1
-#define TAG_CLIENT_MC_RESET_ID 0
-#define TAG_CLIENT_MC_RESET_WIDTH 1
+#include <ps.hpp>
 
 /////////////////////////////////////////////
 // Globals
