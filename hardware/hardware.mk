ee676e580b1f8f23c9669e03b81ccbf56e3bbc6b
Dan Petrisko <petrisko@cs.washington.edu>
2023-09-01 02:35:01 -0700

    Adding HOST_TYPE parameter to configuration rom

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -175,6 +175,7 @@ $(BSG_MACHINE_PATH)/bsg_bladerunner_configuration.rom: $(BSG_MACHINE_PATH)/Makef
 	@echo $(call dec2bin,$(BSG_MACHINE_IO_EP_CREDITS)) >> $@.temp
 	@echo $(call dec2bin,0)                            >> $@.temp
 	@echo $(call hex2bin,$(CHIP_ID))                   >> $@.temp
+	@echo $(call dec2bin,$(BSG_MACHINE_HOST_TYPE))     >> $@.temp
 	@cat $(BSG_MACHINE_PATH)/bsg_bladerunner_memsys.rom >> $@.temp
 	mv $@.temp $@
 
