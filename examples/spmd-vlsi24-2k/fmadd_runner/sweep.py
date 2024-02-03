c2509553ec11f4bc69521ef82fdd859594d7061f
Scott Davidson <scottdavidson1992@gmail.com>
2024-02-02 17:31:40 -0800

    Updates to sweep and makefile

--- examples/spmd-vlsi24-2k/fmadd_runner/sweep.py
+++ examples/spmd-vlsi24-2k/fmadd_runner/sweep.py
@@ -7,30 +7,37 @@ import subprocess
 from hb_board_v0p5_lib import *
 from boot import _boot_wrapper as start_asic
 
-for osc in range(32):
-    print(f"[SWEEP] try osc={osc}")
+fid = open('results.csv', 'w')
+
+print(f"voltage,best_osc", file=fid)
+for v_mv in range(550, 900 + 50, 50):
+    for osc in range(0, 32):
+        v = v_mv / 1000.0
+        print(f"[SWEEP] try volt={v}, osc={osc}")
+
+        time.sleep(1.5)
+        start_asic(
+            voltage=v,
+            pwm=70,
+            clk_0=(True,osc,0),
+            clk_1=(True,osc,0),
+            clk_2=(True,osc,0),
+            clk_3=(True,osc,0),
+        )
+        time.sleep(1.5)
+
+        proc = subprocess.run(["timeout 10s make clean exec.log"], shell=True)
+        if proc.returncode != 0:
+            break
+
     time.sleep(1.5)
     start_asic(
         voltage=0.8,
-        pwm=50,
-        clk_0=(True,osc,0),
-        clk_1=(True,osc,0),
-        clk_2=(True,osc,0),
-        clk_3=(True,osc,0),
+        pwm=20,
+        clk_0=(False,0,0),
+        clk_1=(False,0,0),
+        clk_2=(False,0,0),
+        clk_3=(False,0,0),
     )
-    time.sleep(1.5)
-    try:
-        subprocess.run(["make clean exec.log"], shell=True, timeout=10)
-    except Exception:
-        print(traceback.format_exc())
-        break
 
-time.sleep(1.5)
-start_asic(
-    voltage=0.8,
-    pwm=20,
-    clk_0=(False,0,0),
-    clk_1=(False,0,0),
-    clk_2=(False,0,0),
-    clk_3=(False,0,0),
-)
+    print(f"{v},{osc-1}", file=fid)
