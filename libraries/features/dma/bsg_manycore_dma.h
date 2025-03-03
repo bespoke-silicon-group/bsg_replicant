92935f2e9067498365e256cb42ede16a048783f9
Max Ruttenberg <mrutt@cs.washington.edu>
2025-03-03 10:36:44 -0800

    Revert "allows dma to be disabled at runtime"
    This reverts commit 65b919505cadd4b11b41a5cfa49df26baa03004c.

--- libraries/features/dma/bsg_manycore_dma.h
+++ libraries/features/dma/bsg_manycore_dma.h
@@ -12,8 +12,7 @@
  */
 static inline int hb_mc_dma_supports_write(const hb_mc_manycore_t *mc)
 {
-        return hb_mc_config_memsys_feature_dma(hb_mc_manycore_get_config(mc)) == 1
-                && mc->config.enable_dma == 1;
+        return hb_mc_config_memsys_feature_dma(hb_mc_manycore_get_config(mc)) == 1;
 }
 
 /**
@@ -23,8 +22,7 @@ static inline int hb_mc_dma_supports_write(const hb_mc_manycore_t *mc)
  */
 static inline int hb_mc_dma_supports_read(const hb_mc_manycore_t *mc)
 {
-        return hb_mc_config_memsys_feature_dma(hb_mc_manycore_get_config(mc)) == 1
-            && mc->config.enable_dma == 1;
+        return hb_mc_config_memsys_feature_dma(hb_mc_manycore_get_config(mc)) == 1;
 }
 
 /**
