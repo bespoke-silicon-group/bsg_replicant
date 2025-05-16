d28df554a148dbaca5dc1d9c695d0fa9156308d7
Paul Gao <gaozihou@uw.edu>
2025-05-16 14:10:15 -0700

    Replace python2 with python (#865)

--- libraries/platforms/bigblade-fpga/hardware.mk
+++ libraries/platforms/bigblade-fpga/hardware.mk
@@ -38,7 +38,7 @@ ASCII_TO_ROM_PY = $(BASEJUMP_STL_DIR)/bsg_mem/bsg_ascii_to_rom.py
 
 hardware.configuration: $(BSG_MACHINE_PATH)/bsg_bladerunner_configuration.sv
 $(BSG_MACHINE_PATH)/bsg_bladerunner_configuration.sv: $(BSG_MACHINE_PATH)/bsg_bladerunner_configuration.rom
-	env python2 $(ASCII_TO_ROM_PY) $< bsg_bladerunner_configuration > $@
+	env python $(ASCII_TO_ROM_PY) $< bsg_bladerunner_configuration > $@
 
 .PRECIOUS: $(BSG_MACHINE_PATH)/bsg_bladerunner_configuration.sv
 
